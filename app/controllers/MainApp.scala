c91d5172eb8d0aa4df2b964d6592666697d3ace9
Paul Roberts <paul.roberts@guardian.co.uk>
2016-06-01 13:29:56 +0100

    display an atom

--- app/controllers/MainApp.scala
+++ app/controllers/MainApp.scala
@@ -1,8 +1,11 @@
 package controllers
 
+import com.gu.contentatom.thrift.Atom
+import com.gu.contentatom.thrift.atom.media.MediaAtom
 import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import model.ThriftUtil
+import views.html.MediaAtom._
 
 class MainApp extends Controller {
 
@@ -12,9 +15,9 @@ class MainApp extends Controller {
 
   // takes a configured URL object and shows how it would look as a content atom
 
-  def show = Action(ThriftUtil.bodyParser) { implicit req =>
-    req.body match {
-      case Right(atom) => Ok(atom.toString)
+  def show(uri: String) = Action { implicit req =>
+    new ThriftUtil(Map("uri" -> uri)).parseRequest match {
+      case Right(atom) => Ok(displayAtom(atom))
       case Left(err) => InternalServerError(s"could not parse atom data: $err\n")
     }
   }
