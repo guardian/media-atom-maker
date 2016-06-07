d6822da6fe1a459e9c2fa3d8703ff7a6a8e7cff6
Paul Roberts <paul.roberts@guardian.co.uk>
2016-06-07 17:12:45 +0100

    publish button

--- public/javascript/atom.js
+++ public/javascript/atom.js
@@ -1,6 +1,10 @@
 window.AtomUtil = (function() {
   var ret = {};
 
+  function handleError(xhr) {
+    alert(err + ":" + xhr.responseJSON.error);
+  }
+
   ret.addAsset = function(atomId) {
     var uri = $("#urlInput").val();
     var version = $("#versionInput").val()
@@ -9,7 +13,16 @@ window.AtomUtil = (function() {
       url: "/api/atom/" + atomId + "/asset",
       data: { uri: uri, version: version },
       success: function() { window.location.reload(); },
-      error: function(xhr, status, err) { alert(err + ":" + xhr.responseJSON.error); }
+      error: handleError
+    });
+  }
+
+  ret.publishAtom = function(atomId) {
+    $.ajax({
+      method: "POST",
+      url: "/api/atom/" + atomId + "/publish",
+      success: function() { alert("Published"); },
+      error:   handleError
     });
   }
 
