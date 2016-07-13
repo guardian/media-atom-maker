81801da4441ff20632d4608d67bb60a732d5211b
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2016-07-13 14:10:41 +0100

    update tests

--- test/MediaAtomSuite.scala
+++ test/MediaAtomSuite.scala
@@ -1,7 +1,7 @@
 package test
 
 import com.google.inject.AbstractModule
-import com.gu.atom.publish.AtomPublisher
+import com.gu.atom.publish.{LiveAtomPublisher, PreviewAtomPublisher}
 import play.api.Configuration
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.ws.WSClient
@@ -24,10 +24,11 @@ trait MediaAtomSuite extends PlaySpec with OneAppPerSuite {
   override lazy val app = guicer.build
 
   val oneHour = 3600000L
-  def getApi(dataStore: DataStore, publisher: AtomPublisher) = {
+  def getApi(dataStore: DataStore, livePublisher: LiveAtomPublisher, previewPublisher: PreviewAtomPublisher) = {
     guicer
       .overrides(bind(classOf[DataStore]).toInstance(dataStore))
-      .overrides(bind(classOf[AtomPublisher]).toInstance(publisher))
+      .overrides(bind(classOf[LiveAtomPublisher]).toInstance(livePublisher))
+      .overrides(bind(classOf[PreviewAtomPublisher]).toInstance(previewPublisher))
       .injector
       .instanceOf(classOf[Api])
   }
