03de35a6416b661123fd295722b76a02e7c10d24
Kelvin Chappell <kelvin.chappell@theguardian.com>
2016-07-13 14:45:03 +0100

    Ignore auth in dev environment

--- app/di.scala
+++ app/di.scala
@@ -1,9 +1,13 @@
 import com.google.inject.AbstractModule
+import com.google.inject.Stage.DEVELOPMENT
 import com.gu.pandomainauth.action.AuthActions
 
 class Module extends AbstractModule {
   def configure() = {
-    bind(classOf[AuthActions])
-    .to(classOf[controllers.PanDomainAuthActions])
+    if (currentStage() == DEVELOPMENT) {
+      bind(classOf[AuthActions]).to(classOf[controllers.DevAuthActions])
+    } else {
+      bind(classOf[AuthActions]).to(classOf[controllers.PanDomainAuthActions])
+    }
   }
 }
