fb7d4cc5d2778d2e4913d117f32c989290144373
Paul Roberts <paul.roberts@guardian.co.uk>
2016-07-15 10:17:26 +0100

    more cleanup

--- atom-publisher-lib/src/main/scala/com/gu/atom/publish/AtomReindexer.scala
+++ atom-publisher-lib/src/main/scala/com/gu/atom/publish/AtomReindexer.scala
@@ -3,12 +3,6 @@ package com.gu.atom.publish
 import com.gu.contentatom.thrift.ContentAtomEvent
 import scala.concurrent.{ ExecutionContext, Future }
 
-// sealed trait ReindexResult[A]
-// sealed abstract class ReindexError(msg: String) extends ReindexResult
-// case object JobAlreadyInProgress
-//     extends ReindexError("A job is already in progress")
-// case class Success[A](value: A) extends ReindexResult
-
 sealed trait ReindexJobStatus
 case class Completed(completedCount: Int) extends ReindexJobStatus
 case class InProgress(completedCount: Int) extends ReindexJobStatus
@@ -28,6 +22,5 @@ trait AtomReindexer {
 
   def startReindexJob(atomsToReindex: Iterator[ContentAtomEvent], expectedSize: Int)
                      (implicit ec: ExecutionContext): AtomReindexJob
-  def reindexStatus: Option[AtomReindexJob]
 
 }
