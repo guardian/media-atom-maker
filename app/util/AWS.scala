4b928f2906ee1d58c5e350963a0d339a8f33b95f
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2016-08-16 09:25:58 +0100

    add published dynamo table name to aws conf

--- app/util/AWS.scala
+++ app/util/AWS.scala
@@ -40,6 +40,7 @@ class AWSConfig @Inject() (config: Configuration) {
   )
 
   lazy val dynamoTableName = config.getString("aws.dynamo.tableName").get
+  lazy val publishedDynamoTableName = config.getString("aws.dynamo.publishedTableName").get
 
   lazy val liveKinesisStreamName = config.getString("aws.kinesis.liveStreamName").get
   lazy val previewKinesisStreamName = config.getString("aws.kinesis.previewStreamName").get
