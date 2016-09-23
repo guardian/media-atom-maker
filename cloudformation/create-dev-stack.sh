865a9a0cc94844dee0c5fc67cf1233677c1115a5
Gideon Goldberg <gidsgoldberg@gmail.com>
2016-09-23 12:05:07 +0100

    remove yml-json conversion

--- cloudformation/create-dev-stack.sh
+++ cloudformation/create-dev-stack.sh
@@ -9,16 +9,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 USERNAME=$1
 AWS_PROFILE=$2
 
-${DIR}/convert-yaml-to-json.rb ${DIR}/media-atom-maker-dev.json
-
 aws cloudformation create-stack --stack-name media-atom-maker-DEV-${USERNAME}  \
-    --template-body file://${DIR}/media-atom-maker-dev.json \
-    --parameters  ParameterKey=DynamoDBTablePrefix,ParameterValue=media-atom-maker \
+    --template-body file://${DIR}/media-atom-maker-dev.yml \
     --profile ${AWS_PROFILE} \
     --region eu-west-1
 
 if [ $? -ne 0 ]; then
-    echo "Seems you don't have the correct credentials, try uploading the JSON manualy via the console"
+    echo "Seems you don't have the correct credentials, try uploading the YML manualy via the console"
 else
     echo "Stack media-atom-maker-DEV-${USERNAME} has been created."
 fi
