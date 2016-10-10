34f3206cacff8685b725ae5183e792fceba34983
Shaun Dillon <shaun.dillon@guardian.co.uk>
2016-10-10 16:15:56 +0100

    add atom list route, auth the render action

--- app/controllers/BetaApp.scala
+++ app/controllers/BetaApp.scala
@@ -1,15 +1,15 @@
 package controllers
 
-import play.api.mvc._
-import javax.inject._
+import javax.inject.Inject
+
 import com.gu.pandomainauth.action.AuthActions
 
 class BetaApp @Inject() (val authActions: AuthActions)
-      extends AtomController {
+  extends AtomController {
 
-  def index(id: String = "") = Action {
+  import authActions.{ AuthAction }
 
-      Ok(views.html.BetaApp.app("Media Atom Maker"))
-    }
-
+  def index(id: String) = AuthAction { req =>
+    Ok(views.html.BetaApp.app("Media Atom Maker"))
+  }
 }
