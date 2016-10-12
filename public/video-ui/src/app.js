4ede12089cf82cc995a7eae17e190cf627622caa
Shaun Dillon <shaun.dillon@guardian.co.uk>
2016-10-12 12:16:33 +0100

    add base styles and fonts

--- public/video-ui/src/app.js
+++ public/video-ui/src/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import {Main} from './components/Main/Main';
 
-import '../styles/index.scss';
+import '../styles/main.scss';
 
 const element = document.getElementById('react-mount');
 
