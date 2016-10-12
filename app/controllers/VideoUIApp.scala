9e1ebee06a1fd019d55ea1287859c20ada630e24
Shaun Dillon <shaun.dillon@guardian.co.uk>
2016-10-12 11:03:56 +0100

    spacing in template and route name adjustment

--- app/controllers/VideoUIApp.scala
+++ app/controllers/VideoUIApp.scala
@@ -5,18 +5,18 @@ import javax.inject._
 import com.gu.pandomainauth.action.AuthActions
 
 class VideoUIApp @Inject() (val authActions: AuthActions)
-      extends AtomController {
+  extends AtomController {
 
   def index(id: String = "") = Action {
 
-      val jsFileName = "video-ui/build/app.js"
+    val jsFileName = "video-ui/build/app.js"
 
-      val jsLocation = sys.env.get("JS_ASSET_HOST") match {
-        case Some(assetHost) => assetHost + jsFileName
-        case None => routes.Assets.versioned(jsFileName).toString
-      }
-
-      Ok(views.html.VideoUIApp.app("Media Atom Maker", jsLocation))
+    val jsLocation = sys.env.get("JS_ASSET_HOST") match {
+      case Some(assetHost) => assetHost + jsFileName
+      case None => routes.Assets.versioned(jsFileName).toString
     }
+
+    Ok(views.html.VideoUIApp.app("Media Atom Maker", jsLocation))
+  }
 
 }
