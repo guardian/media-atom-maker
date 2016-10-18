2d0008d5c6d261fd4f666a631da5bbf475c68ea0
Shaun Dillon <shaun.dillon@guardian.co.uk>
2016-10-18 11:52:38 +0100

    add existing video saving

--- public/video-ui/src/components/Video/Display.js
+++ public/video-ui/src/components/Video/Display.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import VideoEdit from '../VideoEdit/VideoEdit';
+import SaveButton from '../utils/SaveButton';
 
 class VideoDisplay extends React.Component {
 
   componentWillMount() {
     this.props.videoActions.getVideo(this.props.params.id);
   }
+
+  saveVideo = () => {
+    this.props.videoActions.saveVideo(this.props.video);
+  };
+
+  updateVideo = (video) => {
+    this.props.videoActions.updateVideo(video);
+  };
 
   render() {
     const video = this.props.video && this.props.params.id === this.props.video.id ? this.props.video : undefined;
@@ -15,12 +25,8 @@ class VideoDisplay extends React.Component {
 
     return (
         <div>
-          <h2>{video.data.title}</h2>
-          <div>
-            <p>Type: {video.type}</p>
-            <p>Revision: {video.contentChangeDetails.revision}</p>
-            <p>No. of Assets: {video.data.assets.length}</p>
-          </div>
+          <VideoEdit videoEditable={this.props.videoEditable} video={this.props.video || {}} updateVideo={this.updateVideo} />
+          <SaveButton onSaveClick={this.saveVideo} />
         </div>
     );
   }
@@ -30,6 +36,7 @@ class VideoDisplay extends React.Component {
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as getVideo from '../../actions/VideoActions/getVideo';
+import * as saveVideo from '../../actions/VideoActions/saveVideo';
 
 function mapStateToProps(state) {
   return {
@@ -39,7 +46,7 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    videoActions: bindActionCreators(Object.assign({}, getVideo), dispatch)
+    videoActions: bindActionCreators(Object.assign({}, getVideo, saveVideo), dispatch)
   };
 }
 
