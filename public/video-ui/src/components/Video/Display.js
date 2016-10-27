7fc7dc3a472320740921af94abb6a719d07eff01
Shaun Dillon <shaun.dillon@guardian.co.uk>
2016-10-27 17:00:18 +0100

    abstract editable state to separate render

--- public/video-ui/src/components/Video/Display.js
+++ public/video-ui/src/components/Video/Display.js
@@ -29,6 +29,21 @@ class VideoDisplay extends React.Component {
     });
   };
 
+  renderDetails = () => {
+    if(this.state.editable) {
+      return (
+        <div className="video__sidebar video-details">
+          <form className="form video__sidebar__group">
+            <VideoEdit video={this.props.video || {}} updateVideo={this.updateVideo}/>
+            <SaveButton onSaveClick={this.saveVideo}/>
+          </form>
+        </div>
+      )
+    } else {
+      return <VideoDetails video={this.props.video || {}} enableEditing={this.enableEditing} />
+    }
+  }
+
   render() {
     const video = this.props.video && this.props.params.id === this.props.video.id ? this.props.video : undefined;
 
@@ -36,32 +51,15 @@ class VideoDisplay extends React.Component {
       return <div className="container">Loading... </div>;
     }
 
-    if(this.state.editable) {
-      return (
-          <div className="video">
-            <VideoAssets video={this.props.video || {}} />
-
-            <VideoPreview video={this.props.video || {}} />
-
-            <div className="video__sidebar video-details">
-              <form className="form">
-                <VideoEdit video={this.props.video || {}} updateVideo={this.updateVideo}/>
-                <SaveButton onSaveClick={this.saveVideo}/>
-              </form>
-            </div>
-          </div>
-      )
-    } else {
-      return (
+    return (
         <div className="video">
           <VideoAssets video={this.props.video || {}} />
 
           <VideoPreview video={this.props.video || {}} />
 
-          <VideoDetails video={this.props.video || {}} enableEditing={this.enableEditing} />
+          {this.renderDetails()}
         </div>
-      )
-    }
+    )
   }
 }
 
