dc924f219e24239681991e5cfeec8759af093e4f
Shaun Dillon <shaun.dillon@guardian.co.uk>
2016-10-28 12:47:03 +0100

    add duration field

--- public/video-ui/src/components/VideoEdit/VideoEdit.js
+++ public/video-ui/src/components/VideoEdit/VideoEdit.js
@@ -2,6 +2,7 @@ import React from 'react';
 import VideoTitleEdit from './formComponents/VideoTitle';
 import VideoCategorySelect from './formComponents/VideoCategory';
 import VideoPosterImageEdit from './formComponents/VideoPosterImage';
+import VideoDurationEdit from './formComponents/VideoDuration';
 import validate from '../../constants/videoEditValidation';
 import { Field, reduxForm } from 'redux-form';
 
@@ -11,6 +12,7 @@ const VideoEdit = (props) => {
           <Field name="title" type="text" component={VideoTitleEdit} {...props} />
           <Field name="category" type="text" component={VideoCategorySelect} {...props} />
           <Field name="posterUrl" type="text" component={VideoPosterImageEdit} {...props} />
+          <Field name="duration" type="number" component={VideoDurationEdit} {...props} />
         </div>
     )
 };
---
+++ public/video-ui/src/components/VideoEdit/formComponents/VideoDuration.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default class VideoDurationEdit extends React.Component {
+
+  onUpdateDuration= (e) => {
+    let newData = Object.assign({}, this.props.video.data, {
+      duration: e.target.value
+    });
+
+    this.props.updateVideo(Object.assign({}, this.props.video, {
+      data: newData
+    }));
+  };
+
+  render () {
+    if (!this.props.video) {
+      console.log('VideoEdit loaded without video provided');
+      return false;
+    }
+
+    const hasError = this.props.meta.touched && this.props.meta.error;
+
+    return (
+        <div className="form__row">
+          <label className="form__label">Duration (in seconds)</label>
+          <input { ...this.props.input} className={"form__field " + (hasError ? "form__field--error" : "")} type="number" value={this.props.video.data.duration || ""} onChange={this.onUpdateDuration} />
+          {hasError ? <p className="form__message form__message--error">{this.props.meta.error}</p> : ""}
+        </div>
+    );
+  }
+}
