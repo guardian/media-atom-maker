0c11760f0110385a0af19cd82b3a49212dd09ddf
Shaun Dillon <shaun.dillon@guardian.co.uk>
2016-10-31 16:45:33 +0000

    extract config from view and apply to state

--- public/video-ui/src/reducers/rootReducer.js
+++ public/video-ui/src/reducers/rootReducer.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import config from './configReducer';
 import error from './errorReducer';
 import video from './videoReducer';
 import videos from './videosReducer';
@@ -6,6 +7,7 @@ import { reducer as form } from 'redux-form'
 
 
 export default combineReducers({
+  config,
   error,
   video,
   videos,
