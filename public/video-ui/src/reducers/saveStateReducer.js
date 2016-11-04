73ff75f1ba9db4fd25c881f2c5f7cfacce12d628
Shaun Dillon <shaun.dillon@guardian.co.uk>
2016-11-04 16:49:18 +0000

    add state reducers for saves and creations

--- public/video-ui/src/reducers/saveStateReducer.js
+++ public/video-ui/src/reducers/saveStateReducer.js
@@ -1,17 +1,36 @@
 import {saveStateVals} from '../constants/saveStateVals'
 
 export default function saveState(state = '', action) {
-}
-switch (action.type) {
-  case 'SHOW_ERROR':
-    return saveStateVals.error;
 
-  case 'VIDEO_CREATE_REQUEST':
-    return saveStateVals.inprogress;
+  switch (action.type) {
+    case 'SHOW_ERROR':
+      return saveStateVals.error;
 
-  case 'VIDEO_CREATE_RECEIVE':
-    return '';
+    case 'VIDEO_CREATE_REQUEST':
+      return saveStateVals.inprogress;
 
-  default:
-    return state;
+    case 'VIDEO_PUBLISH_REQUEST':
+      return saveStateVals.inprogress;
+
+    case 'ASSET_REVERT_REQUEST':
+      return saveStateVals.inprogress;
+
+    case 'VIDEO_SAVE_REQUEST':
+      return saveStateVals.inprogress;
+
+    case 'VIDEO_PUBLISH_RECEIVE':
+      return '';
+
+    case 'VIDEO_CREATE_RECEIVE':
+      return '';
+
+    case 'ASSET_REVERT_RECEIVE':
+      return '';
+
+    case 'VIDEO_SAVE_RECEIVE':
+      return '';
+
+    default:
+      return state;
+  }
 }
