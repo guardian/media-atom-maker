d55c8c5619858d16a8c887c741a07b8f1148df99
Shaun Dillon <shaun.dillon@guardian.co.uk>
2016-11-07 17:05:08 +0000

    add save button state to create and apply loading class

--- public/video-ui/src/components/utils/SaveButton.js
+++ public/video-ui/src/components/utils/SaveButton.js
@@ -1,20 +1,7 @@
 import React from 'react';
-import ProgressSpinner from './ProgressSpinner';
 import {saveStateVals} from '../../constants/saveStateVals';
 
 export default class SaveButton extends React.Component {
-
-  renderSaveStateIndicator = () => {
-    if (this.props.saveState == saveStateVals.inprogress) {
-      return (
-          <div className="save__button--indicator">
-            <ProgressSpinner />
-          </div>
-      );
-    }
-
-    return false;
-  };
 
   renderButtons = () => {
     if (this.props.isHidden) {
@@ -23,13 +10,12 @@ export default class SaveButton extends React.Component {
 
     return (
         <div className="save">
-          <button type="button" className="btn" onClick={this.props.onSaveClick}>
+          <button type="button" className={(this.props.saveState == saveStateVals.inprogress ? "btn--loading " : "") + "btn"} onClick={this.props.onSaveClick}>
             <i className="i-tick-green"/>Save
           </button>
           <button type="button" className="btn" onClick={this.props.onResetClick}>
             <i className="i-cross-red"/>Reset
           </button>
-          {this.renderSaveStateIndicator()}
         </div>
     );
   };
