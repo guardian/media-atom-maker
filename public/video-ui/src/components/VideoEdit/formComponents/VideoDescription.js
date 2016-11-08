ce5a4fbde799213aba4cff02d04c7afe4f26ce66
Shaun Dillon <shaun.dillon@guardian.co.uk>
2016-11-08 15:24:01 +0000

    video description component

---
+++ public/video-ui/src/components/VideoEdit/formComponents/VideoDescription.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export default class VideoDescriptionEdit extends React.Component {
+
+  onUpdateDescription = (e) => {
+    let newData = Object.assign({}, this.props.video.data, {
+      description: e.target.value
+    });
+
+    this.props.updateVideo(Object.assign({}, this.props.video, {
+      data: newData
+    }));
+  };
+
+  render () {
+    if (!this.props.video) {
+      console.log('VideoEdit loaded without video provided');
+      return false;
+    }
+
+    const hasError = this.props.meta.touched && this.props.meta.error;
+
+    return (
+        <div className="form__row">
+          <label className="form__label">Description</label>
+          <textarea { ...this.props.input} className={"form__field " + (hasError ? "form__field--error" : "")} value={this.props.video.data.description || ""} type="text" onChange={this.onUpdateDescription}>
+            {this.props.video.data.description}
+          </textarea>
+          {hasError ? <p className="form__message form__message--error">{this.props.meta.error}</p> : ""}
+        </div>
+    );
+  }
+}
