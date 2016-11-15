40f203df390e8d440820e68840f62878e0c55259
Susie Coleman <susie.coleman@guardian.co.uk>
2016-11-15 16:42:43 +0000

    Remove unneeded flags.

--- public/video-ui/src/components/VideoDetails/ContentFlags.js
+++ public/video-ui/src/components/VideoDetails/ContentFlags.js
@@ -2,44 +2,16 @@ import React, {PropTypes} from 'react';
 
 export default class ContentFlags extends React.Component {
 
-  renderLegallySensitive() {
-    const classes = this.props.video.data.legallySensitive ? "on-btn on-switch" : "flag-btn off-switch";
-    const text = this.props.video.data.legallySensitive ? "On" : "Off";
-    const value = !!this.props.video.data.legallySensitive;
+  renderLegallySensitiveButton() {
+    const classes = this.props.video.legallySensitive ? "on-btn on-switch" : "flag-btn off-switch";
+    const text = this.props.video.legallySensitive ? "On" : "Off";
+    const value = !this.props.video.legallySensitive;
     return <button name="legallySensitive" value={value} className={classes} onClick={this.updateFlag.bind(this)}>{text}</button>
   }
 
-  renderSensitive() {
-    const classes = this.props.video.data.sensitive ? "on-btn on-switch" : "flag-btn off-switch";
-    const text = this.props.video.data.sensitive ? "On" : "Off";
-    const value = !!this.props.video.data.sensitive;
-    return <button name="sensitive" value={value} className={classes} onClick={this.updateFlag.bind(this)}>{text}</button>
-  }
-
-  renderSuppressRelatedContent() {
-    const classes = this.props.video.data.suppressRelatedContent ? "on-btn on-switch" : "flag-btn off-switch";
-    const text = this.props.video.data.suppressRelatedContent ? "On" : "Off";
-    const value = !!this.props.video.data.suppressRelatedContent;
-    return <button name="suppressRelatedContent" value={value} className={classes} onClick={this.updateFlag.bind(this)}>{text}</button>
-  }
-
-  renderBlockAds() {
-    const classes = this.props.video.data.blockAds ? "on-btn on-switch" : "flag-btn off-switch";
-    const text = this.props.video.data.blockAds ? "On" : "Off";
-    const value = !!this.props.video.data.blockAds;
-    return <button name="blockAds" value={value} className={classes} onClick={this.updateFlag.bind(this)}>{text}</button>
-  }
-
   updateFlag(e) {
-    const flagName = e.target.name;
-    const flagValue = e.target.value;
-    const data = Object.assign({}, this.props.video.data, {
-      settings: {[flagName]: flagValue}
-    });
-    const video = Object.assign({}, this.props.video, {
-      data: data
-    });
-    console.log(video)
+    const flag = {[e.target.name]: e.target.value}
+    const video = Object.assign({}, this.props.video, flag);
     this.props.updateVideoFlags(video)
   }
 
@@ -48,34 +20,13 @@ export default class ContentFlags extends React.Component {
       <div>
       <p className="details-list__title">Filters</p>
         <table className="details-list">
-          <tbody>
-            <tr>
-              <td colSpan="2" className="details-list__title">Sensitive</td>
-            </tr>
-            <tr>
-              <td>{this.renderSensitive()}</td>
-              <td className="details-list__field">This content features children, vulnerable people, or is on a topic that is likely to attract online abuse. If unsure, see <a href="https://docs.google.com/document/d/1BvDwS3oEU5lR5ZB0Us6FlTzx31B6dtzrnD3JXgSpYfI/edit?ts=56b85913#" target="_blank">this document.</a></td>
-            </tr>
+          <tbody className="details-list__flags">
             <tr>
               <td colSpan="2" className="details-list__title">Legally sensitive</td>
             </tr>
             <tr>
-              <td>{this.renderLegallySensitive()}</td>
+              <td>{this.renderLegallySensitiveButton()}</td>
               <td className="details-list__field">This content involves active criminal proceedings.</td>
-            </tr>
-            <tr>
-              <td colSpan="2" className="details-list__title">Suppress related content</td>
-            </tr>
-            <tr>
-              <td>{this.renderSuppressRelatedContent()}</td>
-              <td className="details-list__field">Hides related Guardian content and removes from related content results.</td>
-            </tr>
-            <tr>
-              <td colSpan="2" className="details-list__title">Block ads</td>
-            </tr>
-            <tr>
-              <td>{this.renderBlockAds()}</td>
-              <td className="details-list__field">Hides internal modules and adverts.</td>
             </tr>
           </tbody>
         </table>
