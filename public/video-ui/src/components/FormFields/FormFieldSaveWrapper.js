fee755c8b6b284f6d7c7ca507aedd448f5c99fea
Shaun Dillon <shaun.dillon@guardian.co.uk>
2016-11-18 17:12:48 +0000

    add edit button styles and icon

--- public/video-ui/src/components/FormFields/FormFieldSaveWrapper.js
+++ public/video-ui/src/components/FormFields/FormFieldSaveWrapper.js
@@ -39,7 +39,7 @@ export default class FormFieldSaveWrapper extends React.Component {
         {this.fieldWithState()}
         {(this.state.editable ? "EDITING" : "NOT EDITING")}
 
-        {(!this.state.editable ? <button type="button" className="btn" onClick={this.toggleEditing}>Edit</button>: '')}
+        {(!this.state.editable ? <button type="button" className="form__edit-btn" onClick={this.toggleEditing}>Edit</button>: '')}
 
         {(this.state.editable ? <SaveButton saveState={this.props.saveState} onSaveClick={this.saveVideo} onResetClick={this.props.resetVideo} /> : '' )}
       </div>
