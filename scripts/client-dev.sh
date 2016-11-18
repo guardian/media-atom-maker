033f6ce3015b9d7ae730e98cfe996444f2a5c6fe
Akash A <akash1810@gmail.com>
2016-11-18 16:38:22 +0000

    pass `--debug` flag to helper scripts to launch sbt in debug mode

--- scripts/client-dev.sh
+++ scripts/client-dev.sh
@@ -4,4 +4,4 @@ rm public/video-ui/build/main.css
 printf "\n\rStarting Webpack Dev Server... \n\r\n\r"
 npm run client-dev &
 printf "\n\rStarting Play App... \n\r\n\r"
-JS_ASSET_HOST=https://media-atom-maker-assets.local.dev-gutools.co.uk/assets/ sbt run -Dhttp.port=9001
+JS_ASSET_HOST=https://media-atom-maker-assets.local.dev-gutools.co.uk/assets/ ./sbt $@ run
