b32f1a8d5ff8c717af8d951098ade851f592d0f4
Christopher Lloyd <christopher.lloyd@theguardian.com>
2016-11-21 14:55:32 +0000

    Add Youtube Channels endpoint

---
+++ app/model/YouTubeChannel.scala
@@ -0,0 +1,24 @@
+package model
+
+import com.google.api.services.youtube.model.Channel
+import play.api.libs.json._
+
+case class YouTubeChannel(
+                           name: String,
+                           logo: String,
+                           id: String
+                          )
+
+object YouTubeChannel {
+  implicit val reads: Reads[YouTubeChannel] = Json.reads[YouTubeChannel]
+  implicit val writes: Writes[YouTubeChannel] = Json.writes[YouTubeChannel]
+
+  def build(channel: Channel): YouTubeChannel = {
+
+    YouTubeChannel(
+      name = channel.getSnippet().getTitle(),
+      logo = channel.getSnippet().getThumbnails().getDefault().getUrl(),
+      id = channel.getId
+    )
+  }
+}
