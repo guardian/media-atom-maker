c1c5b811e6fde3c876ccb1e4c573caae4ca590c7
Christopher Lloyd <christopher.lloyd@theguardian.com>
2016-12-13 13:06:41 +0000

    Add logs.gutools.co.uk support

---
+++ app/util/LogShipping.scala
@@ -0,0 +1,55 @@
+package util
+
+import javax.inject.{Inject, Singleton}
+
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.classic.{Logger => LogbackLogger}
+import com.gu.logback.appender.kinesis.KinesisAppender
+import net.logstash.logback.layout.LogstashLayout
+import org.slf4j.{LoggerFactory, Logger => SLFLogger}
+import play.api.Logger
+
+
+trait LogShipping {
+  def init(): Unit
+}
+
+
+@Singleton
+class LogShippingImpl @Inject() (val awsConfig: AWSConfig) extends LogShipping {
+  val rootLogger = LoggerFactory.getLogger(SLFLogger.ROOT_LOGGER_NAME).asInstanceOf[LogbackLogger]
+
+  def init() {
+    rootLogger.info("bootstrapping kinesis appender if configured correctly")
+    for (
+      stack <- awsConfig.readTag("Stack");
+      app <- awsConfig.readTag("App");
+      stage <- awsConfig.readTag("Stage");
+      stream <- awsConfig.loggingKinesisStreamName
+    ) {
+
+      Logger.info(s"bootstrapping kinesis appender with $stack -> $app -> $stage")
+      val context = rootLogger.getLoggerContext
+
+      val layout = new LogstashLayout()
+      layout.setContext(context)
+      layout.setCustomFields(s"""{"stack":"$stack","app":"$app","stage":"$stage"}""")
+      layout.start()
+
+      val appender = new KinesisAppender[ILoggingEvent]()
+      appender.setBufferSize(1000)
+      appender.setRegion(awsConfig.region.getName)
+      appender.setStreamName(stream)
+      appender.setContext(context)
+      appender.setLayout(layout)
+
+      appender.start()
+
+      rootLogger.addAppender(appender)
+      rootLogger.info("Configured kinesis appender")
+    }
+  }
+
+  init()
+
+}
