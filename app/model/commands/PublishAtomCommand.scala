a70c8b43ba9806cc23b2ccc502e32b7dc1980d41
Akash A <akash1810@gmail.com>
2017-01-04 22:43:49 +0000

    allow `plutoProjectId` to be set
    by a `PUT` to `/api/atom/:id/update-metadata

--- app/model/commands/PublishAtomCommand.scala
+++ app/model/commands/PublishAtomCommand.scala
@@ -50,8 +50,14 @@ case class PublishAtomCommand(id: String)(implicit val previewDataStore: Preview
           case None => Logger.info(s"No active YouTube asset found for atom: ${id}")
         }
 
-        api.updateMetadata(id, UpdatedMetadata(atom.description, Some(atom.tags), atom.youtubeCategoryId, atom.license,
-          atom.privacyStatus, atom.expiryDate))
+        api.updateMetadata(id, UpdatedMetadata(
+          atom.description,
+          Some(atom.tags),
+          atom.youtubeCategoryId,
+          atom.license,
+          atom.privacyStatus,
+          atom.expiryDate,
+          atom.plutoProjectId))
 
         val changeRecord = ChangeRecord((new Date()).getTime(), None) // Todo: User...
         val updatedAtom = thriftAtom.copy(
