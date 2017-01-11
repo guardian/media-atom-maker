fbcb55d6f040dcb53b3c840f864e4d5c84fe683b
Chris J Clarke <mrmr.mail@gmail.com>
2017-01-11 12:27:29 +0000

    added material icons to search box

--- public/video-ui/src/components/VideoSearch/VideoSearch.js
+++ public/video-ui/src/components/VideoSearch/VideoSearch.js
@@ -13,7 +13,8 @@ export default class VideoSearch extends React.Component {
 
   render () {
     return (
-      <div className="form topbar__search">
+      <div className="topbar__search">
+        <i className="mam-icon search-query__magnifier">search</i>
         <input className={'form__field' + (this.searchInProgress() ? ' form__field--loading' : '')} type="search" value={this.props.searchTerm || ''} onChange={this.onSearch} placeholder={"Search for videos..."} />
       </div>
     )
