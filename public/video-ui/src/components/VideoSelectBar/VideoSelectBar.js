98b922934b7c1519ea6e91e75f6d7fc61ef946ae
Chris J Clarke <mrmr.mail@gmail.com>
2017-01-12 16:50:24 +0000

    step 1 to improve the select video bar

--- public/video-ui/src/components/VideoSelectBar/VideoSelectBar.js
+++ public/video-ui/src/components/VideoSelectBar/VideoSelectBar.js
@@ -3,14 +3,27 @@ import {saveStateVals} from '../../constants/saveStateVals';
 
 export default class VideoSelectBar extends React.Component {
 
-  render() {
-    if (!this.props.embeddedMode) {
-       return false;
+  renderItemImage() {
+    if (!this.props.video) {
+      return  false;
     }
+
+    if (this.props.video.posterImage) {
+      return  <img src={this.props.video.posterImage.master.file} alt={this.props.video.title}/>
+    }
+
+    return <div className="grid__image__placeholder">No Image</div>
+  }
+
+  render() {
 
     return (
       <div className="bar info-bar">
+      <div className="bar__image">{this.renderItemImage()}</div>
+      <div>
+        <span className="grid__item__title">{this.props.video.title}</span>
         <button type="button" className="bar__button" onClick={this.props.onSelectVideo}>Select this Video</button>
+      </div>
       </div>
     )
   }
