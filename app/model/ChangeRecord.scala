07b937e10d382535b6ed5cdb44ba128f4a47e651
Akash A <akash1810@gmail.com>
2017-02-06 19:52:17 +0000

    always set change record
    - setting change record on create sets created and lastModified
    - setting change record on update sets lastModified
    - setting change record on publish sets lastModified and published

--- app/model/ChangeRecord.scala
+++ app/model/ChangeRecord.scala
@@ -3,12 +3,29 @@ package model
 import com.gu.contentatom.thrift.{ChangeRecord => ThriftChangeRecord}
 import org.cvogt.play.json.Jsonx
 import org.joda.time.DateTime
+import com.gu.pandomainauth.model.{User => PandaUser}
+import play.api.libs.json.Format
 
 case class ChangeRecord(date: DateTime, user: Option[User]) {
   def asThrift = ThriftChangeRecord(date.getMillis, user.map(_.asThrift))
 }
 
 object ChangeRecord {
-  implicit val changeRecordFormat = Jsonx.formatCaseClassUseDefaults[ChangeRecord]
+  implicit val changeRecordFormat: Format[ChangeRecord] = Jsonx.formatCaseClassUseDefaults[ChangeRecord]
   def fromThrift(cr: ThriftChangeRecord) = ChangeRecord(new DateTime(cr.date), cr.user.map(User.fromThrift))
+
+  // TODO be better
+  // HACK: HMAC authenticated users are a `PandaUser` without an email
+  // see https://github.com/guardian/media-atom-maker/pull/170
+  private def getUsername (user: PandaUser): String = {
+    user.email match {
+      case "" => user.firstName
+      case _ => user.email
+    }
+  }
+
+  def now (pandaUser: PandaUser): ChangeRecord = {
+    val user = Some(User(getUsername(pandaUser), Some(pandaUser.firstName), Some(pandaUser.lastName)))
+    ChangeRecord(DateTime.now(), user)
+  }
 }
