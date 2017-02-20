b947b33d995520b67ed3183796cc6e30e52b6ad6
Akash A <akash1810@gmail.com>
2017-02-20 17:10:33 +0000

    use correct project in client-dev.sh
    also stop moaning about an optionally non-existing css file

--- scripts/client-dev.sh
+++ scripts/client-dev.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 printf "\n\rRemoving compiled css file... \n\r\n\r"
-rm public/video-ui/build/main.css
+rm public/video-ui/build/main.css 2> /dev/null
 printf "\n\rStarting Webpack Dev Server... \n\r\n\r"
 npm run client-dev &
 printf "\n\rStarting Play App... \n\r\n\r"
-JS_ASSET_HOST=https://video-assets.local.dev-gutools.co.uk/assets/ ./sbt $@ run
+JS_ASSET_HOST=https://video-assets.local.dev-gutools.co.uk/assets/ ./sbt $@ app/run
