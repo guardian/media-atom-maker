2a4056c83395abdafa39d28c82aa48b3b8c85747
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2017-02-23 14:23:44 +0000

    Remove Inject annotation

--- app/controllers/Transcoder.scala
+++ app/controllers/Transcoder.scala
@@ -6,7 +6,7 @@ import com.gu.pandahmac.HMACAuthActions
 import play.api.libs.json.Json
 import play.api.mvc.Controller
 
-class Transcoder @Inject()(val authActions: HMACAuthActions, transcoder: util.Transcoder) extends Controller {
+class Transcoder(val authActions: HMACAuthActions, transcoder: util.Transcoder) extends Controller {
   import authActions.AuthAction
 
   def jobStatus = AuthAction {
