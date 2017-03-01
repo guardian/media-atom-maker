a499f01f515241363580051d09dbce063dcf832b
Michael Barton <michael.barton@guardian.co.uk>
2017-03-01 17:47:53 +0000

    Split into stateless components

--- public/video-ui/src/components/VideoEdit/formComponents/VideoExpiry.js
+++ public/video-ui/src/components/VideoEdit/formComponents/VideoExpiry.js
@@ -1,104 +1,129 @@
 import React from 'react';
 import Picker from 'react-datepicker';
 import moment from 'moment';
+import _ from 'lodash';
 import 'react-datepicker/dist/react-datepicker.css';
+import Icon from '../../Icon';
 
-export default class VideoExpiryEdit extends React.Component {
+function Selector({values, value, disabled, onChange}) {
+  const handler = (e) => {
+    onChange(e.target.value);
+  };
 
-  constructor(props) {
-    super(props);
+  const options = values.map((value) => {
+    return <option key={value} value={value}>{value}</option>;
+  });
 
-    this.fieldName = "Expiry Date";
-    this.validHours = [...new Array(24).keys()];
-    this.validMinutes = [0, 15, 30, 45];
+  const params = {
+    className: "form__field form__field--select",
+    disabled: disabled,
+    value: value,
+    onChange: handler
+  };
 
-    this.readExpiryDate(props);
+  return <select {...params}>{options}</select>;
+}
+
+function HourSelector({date, onChange}) {
+  let values = [" "];
+  if (date) {
+    values = [...new Array(24).keys()].map((hour) => moment().hour(hour).format("HH"));
   }
 
-  componentWillReceiveProps(props) {
-    this.readExpiryDate(props);
+  const params = {
+    values: values,
+    value: date ? date.format("HH") : " ",
+    disabled: !date,
+    onChange: (newHour) => onChange(date.hours(newHour))
+  };
+
+  return <Selector {...params} />;
+}
+
+function MinuteSelector({date, onChange}) {
+  let values = [" "];
+  if (date) {
+    values = [0, 15, 30, 45].map((minute) => moment().minute(minute).format("mm"));
   }
 
-  readExpiryDate(props) {
-    const date = props.video.expiryDate;
-    const midnightToday = moment().hours(0).minutes(0).seconds(0);
+  const params = {
+    values: values,
+    value: date ? date.format("mm") : " ",
+    disabled: !date,
+    onChange: (newMinute) => onChange(date.minutes(newMinute))
+  };
 
-    this.hasExpiryDate = date !== undefined;
-    this.date = date ? moment(date) : midnightToday;
+  return <Selector {...params} />;
+}
+
+function DateSelector({date, onChange}) {
+  const datePickerParams = {
+    className: "form__field",
+    selected: date,
+    minDate: moment(),
+    dateFormat: "DD/MM/YYYY",
+    onChange: (newDate) => {
+      const base = date ? date : moment().hours(0).minutes(0);
+      onChange(newDate.hours(base.hours()).minutes(base.minutes()));
+    }
+  };
+
+  return <Picker {...datePickerParams} />;
+}
+
+function Editor({date, onChange}) {
+  function reset() {
+    onChange(null);
   }
 
-  setExpiryDate(newDate) {
-    const newVideo = Object.assign({}, this.props.video, { expiryDate: newDate.valueOf() });
-    this.props.updateVideo(newVideo);
-  }
-
-  renderDisplay() {
-    const displayString = this.hasExpiryDate ? this.date.format("DD/MM/YYYY HH:mm") : 'No expiry date set';
-
-    return <div>
-      <p className="details-list__title">{this.fieldName}</p>
-      <p className="details-list__field">{displayString}</p>
-    </div>;
-  }
-
-  selector(values, selected, formatter, onChange) {
-    const handler = (e) => {
-      onChange(e.target.value);
-    };
-
-    const options = values.map((value) => {
-      return <option key={value} value={value}>{formatter(value)}</option>;
-    });
-
-    return <select className="form__field form__field--select" value={selected} onChange={handler}>{options}</select>;
-  }
-
-  renderPicker() {
-    const datePickerParams = {
-      className: "form__field",
-      selected: this.date,
-      minDate: moment(),
-      locale: 'en-GB', // for DD/MM/YYYY
-      onChange: (newDate) => {
-        this.setExpiryDate(newDate.hours(this.date.hours()).minutes(this.date.minutes()));
-      }
-    };
-
-    const datePicker = <Picker {...datePickerParams} />;
-
-    const hourPicker = this.selector(this.validHours, this.date.hour(),
-      (hour) => moment().hour(hour).format("HH"),
-      (newHour) => {
-        this.setExpiryDate(this.date.hours(newHour));
-      }
-    );
-
-    const minutePicker = this.selector(this.validMinutes, this.date.minute(),
-      (minute) => moment().minute(minute).format("mm"),
-      (newMinute) => this.setExpiryDate(this.date.minutes(newMinute))
-    );
-
-    return (
+  return (
       <div>
-        <label className="form__label">{this.fieldName}</label>
+        <label className="form__label">Expiry Date</label>
         <div className="expiry-date-picker">
-          <div className="expiry-date-picker__date">{datePicker}</div>
-          <div className="expiry-date-picker__number">{hourPicker}</div>
-          <div className="expiry-date-picker__number">{minutePicker}</div>
+          <div className="expiry-date-picker__date">
+            <DateSelector date={date} onChange={onChange} />
+          </div>
+          <div className="expiry-date-picker__number">
+            <HourSelector date={date} onChange={onChange} />
+          </div>
+          <div className="expiry-date-picker__number">
+            <MinuteSelector date={date} onChange={onChange} />
+          </div>
+          <Icon icon="cancel" className="icon__edit expiry-date-picker__reset" onClick={reset} />
         </div>
       </div>
     );
+}
+
+function Display({date}) {
+  const displayString = date ? date.format("DD/MM/YYYY HH:mm") : 'No expiry date set';
+
+  return <div>
+    <p className="details-list__title">Expiry Date</p>
+    <p className="details-list__field">{displayString}</p>
+  </div>;
+}
+
+export default function VideoExpiryEdit({editable, video, updateVideo}) {
+  function setExpiryDate(newDate) {
+    if(newDate) {
+      const newVideo = Object.assign({}, video, { expiryDate: newDate.valueOf() });
+      updateVideo(newVideo);
+    } else {
+      const newVideo = _.omit(video, 'expiryDate');
+      updateVideo(newVideo);
+    }
   }
 
-  render () {
-    if (!this.props.video) {
-      return false;
-    }
+  if (!video) {
+    return false;
+  }
 
-    if(this.props.editable) {
-      return this.renderPicker();
-    } else {
-      return this.renderDisplay();
-    }
+  const date = video.expiryDate ? moment(video.expiryDate) : null;
+
+  if(editable) {
+    return <Editor date={date} onChange={setExpiryDate} />;
+  } else {
+    return <Display date={date} />;
   }
 }
