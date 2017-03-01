6cc6f6adadc912f0b21f29f0c4a003891b25566c
Michael Barton <michael.barton@guardian.co.uk>
2017-03-01 14:50:01 +0000

    Review fixes

--- uploader/src/main/scala/com/gu/media/DynamoEventsLambda.scala
+++ uploader/src/main/scala/com/gu/media/DynamoEventsLambda.scala
@@ -7,14 +7,14 @@ import scala.collection.JavaConverters._
 class DynamoEventsLambda extends RequestHandler[DynamodbEvent, Unit] {
   override def handleRequest(input: DynamodbEvent, context: Context): Unit = {
     val records = input.getRecords.asScala
-    val log = context.getLogger
+    val logger = context.getLogger
 
     records.foreach { record =>
-      log.log("Received Dynamo notification:")
+      logger.log("Received Dynamo notification:")
 
       val image = record.getDynamodb.getNewImage.asScala
       image.foreach { case(k, v) =>
-        log.log(s"\t$k: $v")
+        logger.log(s"\t$k: $v")
       }
     }
   }
