17a32a419847e9a18f4f0d0ad9fc23efd2c28806
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2017-03-01 09:09:51 +0000

    render pluto videos in a table

--- public/video-ui/src/components/VideoPluto/VideoPlutoList.js
+++ public/video-ui/src/components/VideoPluto/VideoPlutoList.js
@@ -6,8 +6,47 @@ class VideoPlutoList extends React.Component{
     this.props.plutoVideoActions.getPlutoVideos();
   }
 
+  renderPlutoVideo(video) {
+    return (
+      <tr key={video.id}>
+        <td>{video.title}</td>
+        <td>{video.description}</td>
+      </tr>
+    );
+  }
+
+  renderPlutoVideos() {
+    return (
+      <tbody>
+        {this.props.plutoVideos.map(this.renderPlutoVideo)}
+      </tbody>
+    );
+  }
+
+
   render() {
-    return (<div>Hello</div>);
+    if (this.props.plutoVideos) {
+      return (
+        <div>
+          <div className="video__detailbox">
+            <span className="video__detailbox__header">Videos without pluto projects</span>
+          </div>
+          <div>
+            <table className="table">
+              <thead className="table__header">
+                <tr className="table__header-row">
+                  <th>Title</th>
+                  <th>Description</th>
+                </tr>
+              </thead>
+              {this.renderPlutoVideos()}
+            </table>
+          </div>
+        </div>
+      );
+    }
+
+    return (<div>Loading...</div>);
   }
 }
 
