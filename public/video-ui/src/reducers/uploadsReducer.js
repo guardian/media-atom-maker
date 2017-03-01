5db0fa17231c0971bd11efc36ecebb4e7e07f99b
Michael Barton <michael.barton@guardian.co.uk>
2017-03-01 11:12:33 +0000

    Wire UI to new end-points

--- public/video-ui/src/reducers/uploadsReducer.js
+++ public/video-ui/src/reducers/uploadsReducer.js
@@ -1,19 +1,12 @@
-export default function upload(state = { complete: false }, action) {
+export default function upload(state = { handle: null, progress: 0, total: 0 }, action) {
   switch(action.type) {
-    case 'UPLOAD_POLICY_RECEIVE':
-      return { file: action.file, policy: action.policy };
-
-    case 'UPLOAD_POLICY_CLEAR':
-      return { complete: false };
-
-    case 'START_UPLOAD':
-      return { progress: 0.0 };
+    case 'UPLOAD_STARTED': {
+      const total = action.upload.parts[action.upload.parts.length - 1].end;
+      return Object.assign({}, state, { handle: action.handle, total: total });
+    }
 
     case 'UPLOAD_PROGRESS':
-      return { progress: (action.completed * 1.0) / action.total };
-
-    case 'UPLOAD_COMPLETE':
-      return { complete: true };
+      return Object.assign({}, state, { progress: action.progress });
 
     default:
       return state;
--- public/video-ui/src/services/UploadsApi.js
+++ public/video-ui/src/services/UploadsApi.js
@@ -4,38 +4,97 @@ import {pandaReqwest} from './pandaReqwest';
 import 'aws-sdk/dist/aws-sdk';
 const AWS = window.AWS;
 
-class UploadsApi {
-  getUploadPolicy(atomId) {
+class UploadFunctions {
+  createUpload = (atomId, file) => {
     return pandaReqwest({
-      url: `/api2/atom/${atomId}/uploads`,
-      method: 'post'
+      url: `/api2/uploads?atomId=${atomId}`,
+      method: 'post',
+      contentType: 'application/json',
+      data: JSON.stringify({ atomId: atomId, filename: file.name, size: file.size })
     });
-  }
+  };
 
-  getS3(config) {
-    const { temporaryAccessId, temporarySecretKey, sessionToken } = config.credentials;
-    const credentials = new AWS.Credentials(temporaryAccessId, temporarySecretKey, sessionToken);
+  stopUpload = (id) => {
+    return pandaReqwest({
+      url: `/api2/uploads/_stop`,
+      method: 'post',
+      contentType: 'application/json',
+      data: JSON.stringify({ id: id })
+    });
+  };
+
+  uploadPart = (upload, part, file, progressFn) => {
+    const slice = file.slice(part.start, part.end);
+
+    return this.getCredentials(upload.id, part.id).then((credentials) => {
+      const s3 = this.getS3(upload.bucket, upload.region, credentials);
+
+      const params = { Key: part.key, Body: slice, ACL: 'private', Metadata: { original: file.name } };
+      const request = s3.upload(params);
+      
+      request.on('httpUploadProgress', (event) => {
+        progressFn(part.start + event.loaded);
+      });
+      
+      return request;
+    });
+  };
+
+  getCredentials = (id, part) => {
+    return pandaReqwest({
+      url: `/api2/uploads/${id}/_credentials?part=${part}`,
+      method: 'post'
+    }).then((resp) => {
+      return resp;
+    });
+  };
+
+  getS3 = (bucket, region, credentials) => {
+    const { temporaryAccessId, temporarySecretKey, sessionToken } = credentials;
+    const awsCredentials = new AWS.Credentials(temporaryAccessId, temporarySecretKey, sessionToken);
 
     return new AWS.S3({
       apiVersion: '2006-03-01',
-      credentials: credentials,
-      params: { Bucket: config.bucket },
-      region: config.region
+      credentials: awsCredentials,
+      params: { Bucket: bucket },
+      region: region
     });
-  }
-
-  startUpload(config, file, progress) {
-    const s3 = this.getS3(config);
-
-    const params = { Key: config.key, Body: file, ACL: 'private', Metadata: { original: file.name} };
-    const request = s3.upload(params);
-
-    request.on('httpUploadProgress', (event) => {
-      progress(event.loaded, event.total);
-    });
-
-    return request.promise();
-  }
+  };
 }
 
-export default new UploadsApi();
+export const UploadsApi = new UploadFunctions();
+
+export class UploadHandle {
+  constructor(upload, file, progressFn) {
+    this.upload = upload;
+    this.file = file;
+    this.progressFn = progressFn;
+
+    this.request = null;
+  }
+
+  start = () => {
+    this.uploadParts(this.upload.parts);
+  };
+
+  stop = () => {
+    UploadsApi.stopUpload(this.upload.id);
+
+    if(this.request)
+      this.request.abort();
+  };
+
+  uploadParts = (parts) => {
+    if(parts.length > 0) {
+      const partRequest = UploadsApi.uploadPart(this.upload, parts[0], this.file, this.progressFn);
+
+      partRequest.then((s3Request) => {
+        this.request = s3Request;
+
+        s3Request.promise().then(() => {
+          this.uploadParts(parts.slice(1));
+        });
+      });
+    }
+  }
+}
