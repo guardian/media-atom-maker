c16e8a9d229cffa678a74d9cf74a97ca5d04f380
Michael Barton <michael.barton@guardian.co.uk>
2017-03-13 11:15:14 +0000

    Trigger kinesis and update table on upload to S3

--- uploader/src/main/scala/com/gu/media/S3EventsLambda.scala
+++ uploader/src/main/scala/com/gu/media/S3EventsLambda.scala
@@ -1,15 +1,66 @@
 package com.gu.media
 
-import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
+import java.nio.ByteBuffer
+import java.nio.charset.StandardCharsets
+
 import com.amazonaws.services.lambda.runtime.events.S3Event
+import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
+import com.gu.media.aws.{AwsAccess, CrossAccountAccess, DynamoAccess, KinesisAccess}
+import com.gu.media.lambda.LambdaBase
+import com.gu.media.logging.Logging
+import com.gu.media.upload.{DynamoUploadsTable, UploadPartKey}
+
 import scala.collection.JavaConverters._
 
-class S3EventsLambda extends RequestHandler[S3Event, Unit] {
-  override def handleRequest(input: S3Event, context: Context): Unit = {
-    val records = input.getRecords.asScala
+class S3EventsLambda extends RequestHandler[S3Event, Unit]
+  with LambdaBase
+  with Logging
+  with AwsAccess with CrossAccountAccess
+  with DynamoAccess
+  with KinesisAccess {
 
-    records.foreach { record =>
-      context.getLogger.log(s"Received S3 notification for ${record.getS3.getObject.getKey}")
+  val table = new DynamoUploadsTable(this)
+
+  override def handleRequest(input: S3Event, context: Context): Unit = {
+    getKeys(input).foreach { key =>
+      addToStream(key)
+      updateTable(key)
     }
+  }
+
+  def getKeys(input: S3Event): List[UploadPartKey] = {
+    val records = input.getRecords.asScala.toList
+    val keys = records.map(_.getS3.getObject.getKey)
+
+    keys.flatMap {
+      case UploadPartKey(folder, id, part) =>
+        log.info(s"Chunk complete. folder=$folder id=$id part=$part")
+
+        val key = UploadPartKey(folder, id, part)
+        Some(key)
+
+      case other =>
+        log.error(s"Unknown part key format: $other")
+        None
+    }
+  }
+
+  def updateTable(key: UploadPartKey): Unit = {
+    table.get(key.id) match {
+      case Some(before) =>
+        val after = before.withPart(key.part, { part =>
+          part.copy(uploadedToS3 = part.end - part.start)
+        })
+
+        table.put(after)
+
+      case None =>
+        log.error(s"Trigger $key for unknown upload id: ${key.id}")
+    }
+  }
+
+  def addToStream(key: UploadPartKey): Unit = {
+    val bytes = ByteBuffer.wrap(key.toString.getBytes(StandardCharsets.UTF_8))
+    kinesisClient.putRecord(youTubeUploadsStreamName, bytes, key.id)
   }
 }
