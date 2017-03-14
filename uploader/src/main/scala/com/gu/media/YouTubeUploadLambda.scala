fb73c0c07fd1d8ae25e837985cf3ca394cf3e0ca
Michael Barton <michael.barton@guardian.co.uk>
2017-03-14 16:43:07 +0000

    Add asset on upload completion

--- uploader/src/main/scala/com/gu/media/YouTubeUploadLambda.scala
+++ uploader/src/main/scala/com/gu/media/YouTubeUploadLambda.scala
@@ -2,11 +2,15 @@ package com.gu.media
 
 import com.amazonaws.services.lambda.runtime.events.KinesisEvent
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
+import com.amazonaws.services.s3.model.DeleteObjectsRequest
 import com.gu.media.aws.{DynamoAccess, S3Access, UploadAccess}
 import com.gu.media.lambda.LambdaBase
 import com.gu.media.logging.Logging
 import com.gu.media.upload.{DynamoUploadsTable, Upload, UploadPart, UploadPartKey}
 import com.gu.media.youtube.YouTubeAccess
+import com.squareup.okhttp._
+
+import scala.collection.JavaConverters._
 
 class YouTubeUploadLambda extends RequestHandler[KinesisEvent, Unit]
   with LambdaBase
@@ -15,9 +19,14 @@ class YouTubeUploadLambda extends RequestHandler[KinesisEvent, Unit]
   with DynamoAccess
   with YouTubeAccess
   with YouTubeVideoUpload
+  with HmacRequestSupport
   with Logging {
 
-  val table = new DynamoUploadsTable(this)
+  private val table = new DynamoUploadsTable(this)
+  private val domain = sys.env.get("DOMAIN")
+
+  private val http = new OkHttpClient()
+  private val appJson = MediaType.parse("application/json")
 
   override def handleRequest(input: KinesisEvent, context: Context): Unit = {
     for {
@@ -31,6 +40,8 @@ class YouTubeUploadLambda extends RequestHandler[KinesisEvent, Unit]
 
       uploadPart(uploadUri, part.key, part.start, part.end, total, (_: Long) => {}).foreach { videoId =>
         log.info(s"Successful upload ${upload.id}. YouTube ID: $videoId")
+
+        addAsset(upload.metadata.atomId, videoId)
         table.delete(upload.id)
       }
     }
@@ -44,6 +55,34 @@ class YouTubeUploadLambda extends RequestHandler[KinesisEvent, Unit]
 
     table.put(updated)
     url
+  }
+
+  private def addAsset(atomId: String, videoId: String): Unit = {
+    val actuallyPerformRequest = stage != "DEV" && domain.nonEmpty
+
+    val origin = s"${domain.getOrElse("https://video.local.dev-gutools.co.uk")}"
+    val uri = s"$origin//api2/atoms/:id/assets"
+    val hmacHeaders = generateHmacHeaders(uri)
+
+    val videoUri = s"https://www.youtube.com/watch?v=$videoId"
+    val body = s"""{"uri": "$videoUri"}"""
+
+    if(actuallyPerformRequest) {
+      val request = new Request.Builder()
+        .url(uri)
+        .headers(Headers.of(hmacHeaders.asJava))
+        .post(RequestBody.create(appJson, body))
+        .build()
+
+      val response = http.newCall(request).execute()
+      if(response.code() != 200) {
+        log.error(s"Unexpected response adding asset ${response.code()}")
+        log.error(s"uri=$uri body=$body responseBody=${response.body().string()}")
+        log.error(s"atomId=$atomId youTubeId=$videoId")
+      }
+    } else {
+      log.info(s"Add asset: POST $uri $body $hmacHeaders")
+    }
   }
 
   private def readKey(input: KinesisEvent): Option[UploadPartKey] = {
