225a962f003de60333e8f7e7ddb974c5e9a84e3c
Michael Barton <michael.barton@guardian.co.uk>
2017-03-20 15:44:17 +0000

    Common react component for YT embeds

---
+++ public/video-ui/src/components/utils/YouTubeEmbed.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {getStore} from '../../util/storeAccessor';
+
+export default function YouTubeEmbed({id}) {
+    const embedUrl = getStore().getState().config.youtubeEmbedUrl;
+    const src = `${embedUrl}${id}?showinfo=0`;
+
+    return <iframe
+        className="baseline-margin"
+        type="text/html"
+        src={src} allowFullScreen
+        frameBorder="0">
+    </iframe>;
+}
