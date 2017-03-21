0e2772b13ab5a2b26b97c9fd98612e32494a78ec
Michael Barton <michael.barton@guardian.co.uk>
2017-03-21 11:15:51 +0000

    Use video ids instead of time

--- public/video-ui/src/components/utils/YouTubeEmbed.js
+++ public/video-ui/src/components/utils/YouTubeEmbed.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import {getStore} from '../../util/storeAccessor';
 
-export default function YouTubeEmbed({id}) {
+export function youTubeUrl(id) {
+    return `https://www.youtube.com/watch?v=${id}`;
+}
+
+export function YouTubeEmbed({id, className, width, height}) {
     const embedUrl = getStore().getState().config.youtubeEmbedUrl;
     const src = `${embedUrl}${id}?showinfo=0`;
 
     return <iframe
-        className="baseline-margin"
         type="text/html"
+        className={className}
+        width={width}
+        height={height}
         src={src}
         allowFullScreen
         frameBorder="0">
