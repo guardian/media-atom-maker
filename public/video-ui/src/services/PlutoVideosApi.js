648af189d30b21fbf98c396c9b284b0e92e951a3
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2017-03-24 08:37:41 +0000

    more sensible route names

--- public/video-ui/src/services/PlutoVideosApi.js
+++ public/video-ui/src/services/PlutoVideosApi.js
@@ -4,14 +4,14 @@ export default {
 
   fetchPlutoVideos: () => {
     return pandaReqwest({
-      url: '/api2/pluto-list',
+      url: '/api2/pluto',
       method: 'get'
     });
   },
 
   sendToPluto: (atomId, projectId) => {
     return pandaReqwest({
-      url: '/api2/pluto/' + atomId + '/send',
+      url: '/api2/pluto/' + atomId,
       method: 'put',
       contentType: 'application/json',
       data: JSON.stringify({plutoId: projectId})
