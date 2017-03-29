1e05e47dd3ea3e069ba33ba2bccff9536e36ebb9
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2017-03-29 08:57:24 +0100

    fix ation type in reducer

--- public/video-ui/src/reducers/plutoVideosReducer.js
+++ public/video-ui/src/reducers/plutoVideosReducer.js
@@ -3,7 +3,7 @@ export default function plutoVideos(state = [], action) {
 
     case 'PLUTO_VIDEOS_GET_RECEIVE':
       return action.videos || [];
-    case 'ADD_PROJECT_GET_RECEIVE':
+    case 'ADD_PROJECT_RECEIVE':
       return state.filter(video => video.id !== action.videoId);
     default:
       return state;
