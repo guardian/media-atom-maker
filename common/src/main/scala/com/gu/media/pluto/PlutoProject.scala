0984daf35a648bf18e95916b37720a5eda28476a
Akash A <akash1810@gmail.com>
2017-03-30 14:56:47 +0100

    remove redundant Reads/Writes

--- common/src/main/scala/com/gu/media/pluto/PlutoProject.scala
+++ common/src/main/scala/com/gu/media/pluto/PlutoProject.scala
@@ -3,7 +3,6 @@ package com.gu.media.pluto
 import org.cvogt.play.json.Jsonx
 import org.joda.time.DateTime
 import play.api.libs.json._
-import play.api.libs.functional.syntax._
 import com.gu.media.util.JsonDate._
 
 case class PlutoProject (
@@ -16,24 +15,5 @@ case class PlutoProject (
 )
 
 object PlutoProject {
-  implicit val plutoProjectReads: Reads[PlutoProject] = (
-    (JsPath \ "id").read[String] and
-    (JsPath \ "collectionId").read[String] and
-    (JsPath \ "headline").read[String] and
-    (JsPath \ "productionOffice").read[String] and
-    (JsPath \ "status").read[String] and
-    (JsPath \ "created").read[String].map(DateTime.parse)
-  )(PlutoProject.apply _)
-
-  implicit val plutoProjectWrites: Writes[PlutoProject] = (
-    (JsPath \ "id").write[String] and
-    (JsPath \ "collectionId").write[String] and
-    (JsPath \ "headline").write[String] and
-    (JsPath \ "productionOffice").write[String] and
-    (JsPath \ "status").write[String] and
-    (JsPath \ "created").write[String].contramap((_: DateTime).toString)
-  )(unlift(PlutoProject.unapply))
-
-
   implicit val format: Format[PlutoProject] = Jsonx.formatCaseClass[PlutoProject]
 }
