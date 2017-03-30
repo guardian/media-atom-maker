6cce068c3f0a39dada9aa88b8656110784c2dd84
Akash A <akash1810@gmail.com>
2017-03-30 14:15:42 +0100

    logging

--- common/src/main/scala/com/gu/media/pluto/PlutoProjectDataStore.scala
+++ common/src/main/scala/com/gu/media/pluto/PlutoProjectDataStore.scala
@@ -2,13 +2,16 @@ package com.gu.media.pluto
 
 import com.amazonaws.services.dynamodbv2.model.PutItemResult
 import com.gu.media.aws.DynamoAccess
+import com.gu.media.logging.Logging
 import com.gu.scanamo.syntax._
 import com.gu.scanamo.{Scanamo, Table}
 import com.gu.scanamo.DynamoFormat._
 import com.gu.scanamo.DynamoFormat
 import org.joda.time.{DateTime, DateTimeZone}
 
-class PlutoProjectDataStore(aws: DynamoAccess) {
+case class PlutoProjectDataStoreException(err: String) extends Exception(err)
+
+class PlutoProjectDataStore(aws: DynamoAccess) extends Logging {
   implicit val dateTimeFormat = DynamoFormat.coercedXmap[DateTime, String, IllegalArgumentException](
     DateTime.parse(_).withZone(DateTimeZone.UTC)
   )(_.toString)
@@ -30,6 +33,7 @@ class PlutoProjectDataStore(aws: DynamoAccess) {
   }
 
   def put(plutoProject: PlutoProject): PutItemResult = {
+    log.info(s"saving pluto project ${plutoProject.id}")
     val op = table.put(plutoProject)
     Scanamo.exec(aws.dynamoDB)(op)
   }
@@ -41,15 +45,22 @@ class PlutoProjectDataStore(aws: DynamoAccess) {
     result.map {
       case Right(project) => project
       case Left(err) => {
-        throw new Exception(err.toString)
+        log.info(s"pluto project $id not found")
+        throw PlutoProjectDataStoreException(err.toString)
       }
     }
   }
 
   def update(id: String, plutoProject: PlutoProject): Option[PutItemResult] = {
     get(id) match {
-      case Some(_) => Some(put(plutoProject))
-      case None => None
+      case Some(_) => {
+        log.info(s"updating pluto project $id")
+        Some(put(plutoProject))
+      }
+      case None => {
+        log.error(s"failed to update pluto project $id as it does not exist")
+        None
+      }
     }
   }
 }
