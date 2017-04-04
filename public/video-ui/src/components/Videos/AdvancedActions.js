30d215e3c3be53461f6aa8dff819fea967c6ce36
Michael Barton <michael.barton@guardian.co.uk>
2017-04-04 11:04:06 +0100

    Hide actions on video page entirely with no permission

--- public/video-ui/src/components/Videos/AdvancedActions.js
+++ public/video-ui/src/components/Videos/AdvancedActions.js
@@ -2,9 +2,12 @@ import React from 'react';
 import {getStore} from '../../util/storeAccessor';
 
 class AdvancedActions extends React.Component {
+    // the permissions are also validated on the server-side for each request
+    permissions = getStore().getState().config.permissions;
+    showActions = this.permissions.deleteAtom;
+
     renderDelete() {
-        // the permissions are also validated on the server-side for each request
-        if(!getStore().getState().config.permissions.deleteAtom) {
+        if(!this.permissions.deleteAtom) {
             return false;
         }
 
@@ -12,18 +15,22 @@ class AdvancedActions extends React.Component {
             this.props.videoActions.deleteVideo(this.props.video);
         };
 
-        return <li className="asset-list__item">
-            <button className="btn label__expired" onClick={doDelete}>DELETE</button>
-            Usages will not be replaced automatically
+        return <li className="action-list__item">
+            <button className="btn label__expired action-list__button" onClick={doDelete}>DELETE</button>
+            <span className="right">Usages will not be replaced and the video will remain on YouTube</span>
         </li>;
     }
 
     render() {
+        if(!this.showActions) {
+            return false;
+        }
+
         return <div>
             <div className="video__detailbox__header__container">
                 <span className="video__detailbox__header">Advanced</span>
             </div>  
-            <ul className="asset-list">
+            <ul className="action-list">
                 {this.renderDelete()}
             </ul>
         </div>;
