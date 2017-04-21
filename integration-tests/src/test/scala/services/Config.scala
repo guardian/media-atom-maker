7b72b2bdf5317ca6862af298d3e109d53bba6afa
jharewinton <jonathan.hare-winton@guardian.co.uk>
2017-04-21 11:32:54 +0100

    make config change non-breaking

--- integration-tests/src/test/scala/services/Config.scala
+++ integration-tests/src/test/scala/services/Config.scala
@@ -11,14 +11,14 @@ object Config {
   val stage: String = Try(sys.env("INT_TEST_TARGET")).getOrElse("CODE")
 
   /* Test */
-  lazy val targetBaseUrl = if (stage == "PROD") config.getString("targetBaseUrlProd") else config.getString("targetBaseUrlCode")
+  lazy val targetBaseUrl = if (stage == "PROD") config.getString("targetBaseUrlProd") else config.getString("targetBaseUrl")
   lazy val asset = config.getString("asset")
   lazy val assetId = config.getString("assetId")
   lazy val channelId = config.getString("channelId")
   lazy val youtubeCategoryId = config.getString("youtubeCategoryId")
 
   /* Panda */
-  lazy val domain = if (stage == "PROD") config.getString("domain-prod") else config.getString("domain-code")
+  lazy val domain = if (stage == "PROD") config.getString("domain-prod") else config.getString("domain")
   lazy val system = config.getString("system")
   lazy val userFirstName = config.getString("userFirstName")
   lazy val userSecondName = config.getString("userSecondName")
