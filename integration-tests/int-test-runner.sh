2d968be8e99793940cf3fda082b849f23be5fc7f
jharewinton <jonathan.hare-winton@guardian.co.uk>
2017-04-21 14:20:07 +0100

    louder int test alerts on production

--- integration-tests/int-test-runner.sh
+++ integration-tests/int-test-runner.sh
@@ -6,7 +6,7 @@ if [ $STATUS -eq 1 ]
 then
   if [ "$INT_TEST_TARGET" == "PROD" ]
   then
-    curl -X POST --data-urlencode 'payload={"text": "Media Atom Maker integration tests have failed on PRODUCTION"}' ${SLACK_URL}
+    curl -X POST --data-urlencode 'payload={"text": "<!here> Media Atom Maker integration tests have failed on PRODUCTION"}' ${SLACK_URL}
   else
     curl -X POST --data-urlencode 'payload={"text": "Media Atom Maker integration tests have failed on CODE"}' ${SLACK_URL}
   fi
