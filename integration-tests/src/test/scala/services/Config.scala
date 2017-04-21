c48da3867f11f3691f768294cbe0cf75f1cc796a
jharewinton <jonathan.hare-winton@guardian.co.uk>
2017-04-21 11:31:27 +0100

    integration test config support production testing

--- integration-tests/src/test/scala/services/Config.scala
+++ integration-tests/src/test/scala/services/Config.scala
@@ -2,19 +2,23 @@ package integration.services
 
 import com.typesafe.config.ConfigFactory
 
+import scala.util.Try
+
 object Config {
 
   val config = ConfigFactory.load()
 
+  val stage: String = Try(sys.env("INT_TEST_TARGET")).getOrElse("CODE")
+
   /* Test */
-  lazy val targetBaseUrl = config.getString("targetBaseUrl")
+  lazy val targetBaseUrl = if (stage == "PROD") config.getString("targetBaseUrlProd") else config.getString("targetBaseUrlCode")
   lazy val asset = config.getString("asset")
   lazy val assetId = config.getString("assetId")
   lazy val channelId = config.getString("channelId")
   lazy val youtubeCategoryId = config.getString("youtubeCategoryId")
 
   /* Panda */
-  lazy val domain = config.getString("domain")
+  lazy val domain = if (stage == "PROD") config.getString("domain-prod") else config.getString("domain-code")
   lazy val system = config.getString("system")
   lazy val userFirstName = config.getString("userFirstName")
   lazy val userSecondName = config.getString("userSecondName")
