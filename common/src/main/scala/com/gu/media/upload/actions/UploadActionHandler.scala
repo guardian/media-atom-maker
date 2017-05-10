5243046bf666d2b852b6e3e09fbc2c2181953109
Michael Barton <michael.barton@guardian.co.uk>
2017-05-10 18:08:46 +0100

    Transcoding (shared code)

--- common/src/main/scala/com/gu/media/upload/actions/UploadActionHandler.scala
+++ common/src/main/scala/com/gu/media/upload/actions/UploadActionHandler.scala
@@ -1,7 +1,5 @@
 package com.gu.media.upload.actions
 
-import com.amazonaws.regions.{Region, Regions}
-import com.amazonaws.services.elastictranscoder.model.{CreateJobOutput, CreateJobRequest, JobInput}
 import com.gu.media.PlutoDataStore
 import com.gu.media.aws.AwsAccess.UploaderAccess
 import com.gu.media.logging.Logging
@@ -14,10 +12,7 @@ abstract class UploadActionHandler(store: UploadsDataStore, plutoStore: PlutoDat
 
   private val s3 = new S3UploadActions(uploaderAccess.s3Client)
   private val pluto = new PlutoUploadActions(uploaderAccess)
-
-  private val transcoderClient = uploaderAccess.transcoderClient
-  transcoderClient.setRegion(Region.getRegion(Regions.EU_WEST_1))
-  private val TRANSCODER_PRESET_ID = "1351620000001-000001" //System preset: Generic 1080p
+  private val transcoder = new TranscodeUploadActions(uploaderAccess.transcoderClient)
 
   // Returns the new version number
   def addAsset(atomId: String, videoId: String): Long
@@ -50,30 +45,11 @@ abstract class UploadActionHandler(store: UploadsDataStore, plutoStore: PlutoDat
       }
 
     case UploadPartsToSelfHost(upload, key, pipelineId) =>
-      transcodeToS3(key, pipelineId)
+      transcoder.transcodeToS3(key, pipelineId)
 
     case DeleteParts(upload) =>
       s3.deleteParts(upload)
       store.delete(upload.id)
 
-  }
-
-  private def transcodeToS3(fileName: String, pipelineId: String) {
-
-    val jobInput = new JobInput().withKey(fileName)
-
-    val mp4FileName = fileName + ".mp4"
-
-    val jobOutput = new CreateJobOutput()
-      .withKey(mp4FileName)
-      .withPresetId(TRANSCODER_PRESET_ID)
-
-    val createJobRequest = new CreateJobRequest()
-      .withPipelineId(pipelineId)
-      .withInput(jobInput)
-      .withOutput(jobOutput)
-
-    transcoderClient.createJob(createJobRequest).getJob
-    log.info(s"Sent $fileName to transcoder output will be $mp4FileName")
   }
 }
