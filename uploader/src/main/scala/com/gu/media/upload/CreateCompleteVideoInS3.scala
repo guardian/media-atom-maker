83b2f463fadc99be32a5f159abf23a68a95f0c1b
Michael Barton <michael.barton@guardian.co.uk>
2017-05-11 11:25:06 +0100

    Forgot to create complete S3 object

--- uploader/src/main/scala/com/gu/media/upload/CreateCompleteVideoInS3.scala
+++ uploader/src/main/scala/com/gu/media/upload/CreateCompleteVideoInS3.scala
@@ -1,13 +1,15 @@
 package com.gu.media.upload
 
+import com.gu.media.aws.S3Access
 import com.gu.media.lambda.LambdaWithParams
 import com.gu.media.logging.Logging
 import com.gu.media.upload.model.Upload
 
-class CreateCompleteVideoInS3 extends LambdaWithParams[Upload, Upload] with Logging {
-  override def handle(upload: Upload): Upload = {
-    log.info(s"Creating complete video ${upload.metadata.pluto.s3Key} in S3 (not really!!)")
+class CreateCompleteVideoInS3 extends LambdaWithParams[Upload, Upload] with S3Access with Logging{
+  private val actions = new S3UploadActions(this.s3Client)
 
+  override def handle(upload: Upload): Upload = {
+    actions.createCompleteObject(upload, upload.metadata.pluto.s3Key)
     upload
   }
 }
