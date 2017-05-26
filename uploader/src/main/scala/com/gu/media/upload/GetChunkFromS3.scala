cf55e9ac87522d6c69868de2972358aece1c0b6a
Michael Barton <michael.barton@guardian.co.uk>
2017-05-26 10:23:29 +0100

    Fix progress reporting

--- uploader/src/main/scala/com/gu/media/upload/GetChunkFromS3.scala
+++ uploader/src/main/scala/com/gu/media/upload/GetChunkFromS3.scala
@@ -1,18 +1,23 @@
 package com.gu.media.upload
 
-import com.gu.media.aws.S3Access
+import com.gu.media.aws.{DynamoAccess, S3Access}
 import com.gu.media.lambda.LambdaWithParams
 import com.gu.media.upload.model.{Upload, UploadPart}
 
-class GetChunkFromS3 extends LambdaWithParams[Upload, Upload] with S3Access {
+class GetChunkFromS3 extends LambdaWithParams[Upload, Upload] with S3Access with DynamoAccess {
+  private val table = new UploadsDataStore(this)
+
   override def handle(upload: Upload): Upload = {
     val chunk = upload.parts(upload.progress.chunksInS3)
 
-    if(s3Client.doesObjectExist(upload.metadata.bucket, chunk.key)) {
+    val updated = if(s3Client.doesObjectExist(upload.metadata.bucket, chunk.key)) {
       complete(upload, chunk)
     } else {
       retry(upload, chunk)
     }
+
+    table.report(updated)
+    updated
   }
 
   private def complete(upload: Upload, chunk: UploadPart): Upload = {
