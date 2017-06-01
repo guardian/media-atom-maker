44e29038ff488af52d07f311bc6d480ce6d98cfa
Michael Barton <michael.barton@guardian.co.uk>
2017-06-01 10:20:39 +0100

    Assignment in wrong place

--- public/video-ui/src/reducers/uploadsReducer.js
+++ public/video-ui/src/reducers/uploadsReducer.js
@@ -1,16 +1,17 @@
 import _ from 'lodash';
 
 export default function uploads(state = [], action) {
-  const id = action.upload.id;
-
   switch (action.type) {
-    case 'UPLOAD_STARTED':
+    case 'UPLOAD_STARTED': {
+      const id = action.upload.id;
+
       if (!_.find(state, upload => upload.id === id)) {
         const status = { id: id, status: 'Uploading', failed: false };
         return [status, ...state];
       }
 
       return state;
+    }
 
     case 'RUNNING_UPLOADS':
       return action.uploads;
