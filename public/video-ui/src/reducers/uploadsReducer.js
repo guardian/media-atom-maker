145e4fd431619a7b1c74c9d7fa828064051d11c3
Michael Barton <michael.barton@guardian.co.uk>
2017-06-01 10:02:53 +0100

    Use placeholder upload instead of React lifecycle method

--- public/video-ui/src/reducers/uploadsReducer.js
+++ public/video-ui/src/reducers/uploadsReducer.js
@@ -1,5 +1,17 @@
+import _ from 'lodash';
+
 export default function uploads(state = [], action) {
+  const id = action.upload.id;
+
   switch (action.type) {
+    case 'UPLOAD_STARTED':
+      if (!_.find(state, upload => upload.id === id)) {
+        const status = { id: id, status: 'Uploading', failed: false };
+        return [status, ...state];
+      }
+
+      return state;
+
     case 'RUNNING_UPLOADS':
       return action.uploads;
 
