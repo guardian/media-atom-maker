c5953c9ce04e23040a41e43afac54e85ef96675a
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2017-06-29 15:11:34 +0100

    add instructions for adding bylines

--- public/video-ui/src/components/FormFields/ComposerTagPicker.js
+++ public/video-ui/src/components/FormFields/ComposerTagPicker.js
@@ -270,6 +270,16 @@ export default class ComposerTagPicker extends React.Component {
     );
   }
 
+  renderBylineInstructions() {
+    if (this.props.tagType === 'contributor') {
+      return (
+        <span className="form__field__instructions">
+          Press space to add byline as text
+        </span>
+      );
+    }
+  }
+
   render() {
     if (!this.props.editable) {
       if (!this.state.tagValue || this.state.tagValue.length === 0) {
@@ -301,6 +311,7 @@ export default class ComposerTagPicker extends React.Component {
 
         <div className="form__label__layout">
           <label className="form__label">{this.props.fieldName}</label>
+          {this.renderBylineInstructions()}
         </div>
         {this.renderCapiUnavailable()}
 
