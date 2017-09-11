c0d8f34c4bf296afe0c1631edf8b5ae1b0b7e37b
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2017-09-11 12:19:59 +0100

    add keywords api

---
+++ public/video-ui/src/services/KeywordsApi.js
@@ -0,0 +1,12 @@
+import { pandaReqwest } from './pandaReqwest';
+import ContentApi from './capi';
+
+export default {
+  composerTagToYouTube: (tagId) => {
+    const encodedId = encodeURIComponent(tagId)
+    return pandaReqwest({
+      url: '/api2/youtube/content-bundle' + encodedId,
+      method: 'get'
+    });
+  }
+}
