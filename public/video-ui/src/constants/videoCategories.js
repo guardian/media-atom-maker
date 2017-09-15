593b7005eaca9be0a475aef8652658943d617a4b
Akash A <akash1810@gmail.com>
2017-09-15 11:38:14 +0100

    temporarily remove Paid category

--- public/video-ui/src/constants/videoCategories.js
+++ public/video-ui/src/constants/videoCategories.js
@@ -9,8 +9,7 @@ const editorialCategories = [
 // title is for ui
 // see https://github.com/guardian/content-atom/blob/master/thrift/src/main/thrift/atoms/media.thrift#L22-L29
 const labsCategories = [
-  {id: 'Hosted', title: 'GLabs - Hosted by'},
-  {id: 'Paid', title: 'GLabs - Paid for'}
+  {id: 'Hosted', title: 'GLabs - Hosted by'}
 ];
 
 export const videoCategories = [
