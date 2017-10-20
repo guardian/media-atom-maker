9f0808902c564a2d32d37c5dce4e057ae5a73325
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2017-10-20 13:26:02 +0100

    composer data as object

--- public/video-ui/src/util/getComposerData.js
+++ public/video-ui/src/util/getComposerData.js
@@ -5,63 +5,28 @@ function asBooleanString(value) {
   return value ? 'true' : 'false';
 }
 
-export function getComposerData(video, videoBlock) {
-  const coreFields = [
-    {
-      'headline': video.title
-    },
-    {
-      'standfirst': video.description ? video.description : null
-    },
-    {
-      'trailText': video.trailText ? video.trailText : null
-    },
-    {
-      'linkText': video.title
-    },
-    {
-      'sensitive': asBooleanString(video.sensitive)
-    },
-    {
-      'legallySensitive': asBooleanString(video.legallySensitive)
-    },
-    {
-      'blockAds': asBooleanString(video.blockAds)
-    },
-    {
-      'commentable': asBooleanString(video.composerCommentsEnabled)
-    },
-    {
-      'relatedContentOff': asBooleanString(video.suppressRelatedContent)
-    },
-    {
-      'seoOptimised': asBooleanString(video.optimisedForWeb)
-    },
-    {
-      'byline': video.byline.join('|')
-    },
-    {
-      'commissioningDesks': video.commissioningDesks.join('|')
-    },
-    {
-      'keywords': video.keywords.join('|')
-    },
-    {
-      'thumbnail': (video.trailImage && video.trailImage.assets.length > 0)
-        ? parseComposerDataFromImage(video.trailImage, video.trailText)
-        : null
-    },
-    {
-      'expiryDate': video.expiryDate
-    }
-  ];
-
+export function getComposerData(video) {
   const isTrainingMode = getStore().getState().config.isTrainingMode;
-
-  // block training content being published
-  const embargoedIndefinately = {
-    'embargoedIndefinitely': asBooleanString(true)
+  return {
+    headline: video.title,
+    standfirst: video.description ? video.description : null,
+    trailText: video.trailText ? video.trailText : null,
+    linkText: video.title,
+    sensitive: asBooleanString(video.sensitive),
+    legallySensitive: asBooleanString(video.legallySensitive),
+    blockAds: asBooleanString(video.blockAds),
+    commentable: asBooleanString(video.composerCommentsEnabled),
+    relatedContentOff: asBooleanString(video.suppressRelatedContent),
+    seoOptimised: asBooleanString(video.optimisedForWeb),
+    commissioningDesks: video.commissioningDesks.join('|'),
+    byline: video.byline.join('|'),
+    keywords: video.keywords.join('|'),
+    thumbnail: video.trailImage && video.trailImage.assets.length > 0
+      ? parseComposerDataFromImage(video.trailImage, video.trailText)
+      : null,
+    expiryDate: video.expiryDate,
+    embargoedIndefinitely: isTrainingMode
+      ? asBooleanString(true)
+      : asBooleanString(false)
   };
-
-  return !isTrainingMode ? coreFields : [...coreFields, embargoedIndefinately];
 }
