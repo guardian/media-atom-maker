ab7d7f960eaa69a7e10ba78e8c5ae215944b5197
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2017-10-23 14:29:32 +0100

    count commas and quotation marks

--- public/video-ui/src/util/getYouTubeTagCharCount.js
+++ public/video-ui/src/util/getYouTubeTagCharCount.js
@@ -1,6 +1,20 @@
 export function getYouTubeTagCharCount(tags) {
   if (Array.isArray(tags)) {
-    return tags.reduce((charCount, keyword) => charCount += keyword.length, 0);
+    const charCount = tags.reduce((charCount, keyword) => {
+
+      //If there is a space in the keyword, youtube adds quotation marks
+      //around the keyword and counts these as spaces
+      if (/\s/g.test(keyword)) {
+        charCount += 2;
+      }
+      return charCount += keyword.length;
+    }, 0);
+
+    //Count commas added between keywords
+    if (tags.length > 0) {
+      return charCount + tags.length - 1;
+    }
+    return charCount;
   }
   return 0;
 }
