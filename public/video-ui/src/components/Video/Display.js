fca488806e2a7e7313c3fbfab4c466a0be59567a
Antonio Voce <antonio.voce@guardian.co.uk>
2017-10-31 14:58:04 +0000

    Reset video usages reducer on atom creation

--- public/video-ui/src/components/Video/Display.js
+++ public/video-ui/src/components/Video/Display.js
@@ -22,6 +22,7 @@ class VideoDisplay extends React.Component {
     if (this.props.route.mode === 'create') {
       this.props.videoActions.updateVideo(blankVideoData);
       this.props.videoActions.updateVideoEditState(true);
+      this.props.videoActions.getUsages();
     } else {
       this.props.videoActions.getVideo(this.props.params.id);
       this.props.videoActions.getUsages(this.props.params.id);
