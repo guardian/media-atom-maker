4cbb06c04d2db469b5cf5e21ac4cc16b072c1a6c
RichieAHB <richard.beddington@guardian.co.uk>
2018-02-01 16:08:58 +0000

    Remove unused actions

--- public/video-ui/src/reducers/targetingReducer.js
+++ public/video-ui/src/reducers/targetingReducer.js
@@ -2,6 +2,10 @@
 // been fetched but there are none
 export default function targeting(state = { targets: null }, action) {
   switch (action.type) {
+    // when we start a new request for targets reset to our 'loading' state
+    case 'TARGETING_GET_REQUEST':
+    case 'TARGETING_POST_REQUEST':
+      return { targets: null };
     // if we receive any targets, for now, completely overwrite what's in here
     case 'TARGETING_POST_RECEIVE':
     case 'TARGETING_GET_RECEIVE':
