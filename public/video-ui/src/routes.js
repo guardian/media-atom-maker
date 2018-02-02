eecb86fa0a5d278e2a1eae3f0fd72da788ea4faf
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2018-02-02 10:21:11 +0000

    Remove auditing from client side code

--- public/video-ui/src/routes.js
+++ public/video-ui/src/routes.js
@@ -3,7 +3,6 @@ import { Router, Route, browserHistory, IndexRedirect, Redirect } from 'react-ro
 
 import Videos from './components/Videos/Videos';
 import VideoDisplay from './components/Video/Display';
-import VideoAuditTrail from './components/VideoAuditTrail/VideoAuditTrail';
 import VideoUpload from './components/VideoUpload/VideoUpload';
 import VideoPlutoList from './components/VideoPluto/VideoPlutoList';
 import Help from './components/Help/Help';
@@ -18,7 +17,6 @@ export const routes = (
       <Redirect from="/videos/create" to="/create" />
       <Route path="/videos/pluto-list" component={VideoPlutoList} />
       <Route path="/videos/:id" component={VideoDisplay} />
-      <Route path="/videos/:id/audit" component={VideoAuditTrail} />
       <Route path="/videos/:id/upload" component={VideoUpload} />
       <Route path="/create" component={VideoDisplay} mode="create" />
       <Route path="/help" component={Help} />
