8698176994ddda46e936074c79c1f1510f6cd6d2
RichieAHB <richard.beddington@guardian.co.uk>
2018-02-02 10:56:25 +0000

    Tidy up reducer

--- public/video-ui/src/reducers/targetingReducer.js
+++ public/video-ui/src/reducers/targetingReducer.js
@@ -1,6 +1,9 @@
 // null here means targets haven't been fetched, an empty array means they have
 // been fetched but there are none
-export default function targeting(state = { targets: null, deleting: [] }, action) {
+export default function targeting(
+  state = { targets: null, deleting: [] },
+  action
+) {
   switch (action.type) {
     case 'TARGETING_UPDATE_REQUEST':
       return {
@@ -16,10 +19,11 @@ export default function targeting(state = { targets: null, deleting: [] }, actio
       return { targets: null, deleting: [] };
     // if we receive any targets, for now, completely overwrite what's in here
     case 'TARGETING_POST_RECEIVE':
+    // v--- fallthrough ---v
     case 'TARGETING_GET_RECEIVE':
       return {
         ...state,
-        targets: (state.targets || []).concat(action.targets)
+        targets: [...(state.targets || []), ...action.targets]
       };
     case 'TARGETING_DELETE_REQUEST':
       return {
@@ -32,9 +36,9 @@ export default function targeting(state = { targets: null, deleting: [] }, actio
         targets: [...state.targets.filter(({ id }) => id !== action.target.id)]
       };
     case 'TARGETING_DELETE_FAILURE':
-    return {
-      deleting: [...state.deleting.filter(id => id !== action.target.id)],
-    };
+      return {
+        deleting: [...state.deleting.filter(id => id !== action.target.id)]
+      };
     default:
       return state;
   }
