77f7d9640cb3e61f1b51f304ff0a1759898cdd05
Andy Gallagher <andy.gallagher@theguardian.com>
2018-08-21 13:35:00 +0100

    if a resync request is sent without a version being present, return a 404 with a descriptive error message

--- app/controllers/PlutoController.scala
+++ app/controllers/PlutoController.scala
@@ -51,16 +51,16 @@ class PlutoController(
   }
 
   def resendAtomMessage(id: String) = APIHMACAuthAction {
-
     try {
       val atomContent = getPreviewAtom(id)
       val atom = MediaAtom.fromThrift(atomContent)
-      val versionWithId = MediaAtomHelpers.getCurrentAssetVersion(atom) match {
-        case Some(versionNumber)=>id + s"-$versionNumber"
-        case None=>
-          log.error("Requested re-index on an atom with no currentAssetVersion, this could indicate a problem.")
-          id
+
+      if(MediaAtomHelpers.getCurrentAssetVersion(atom).isEmpty){
+        log.warn("Requested re-index on an atom with no currentAssetVersion, returning 404")
+        NotFound(Json.toJson(Map("status"->"notfound", "detail"->"atom had no current version, resync was probably sent too early")))
       }
+
+      val versionWithId = id + s"-${MediaAtomHelpers.getCurrentAssetVersion(atom).get}"
 
       atom.plutoData match {
         case Some(data)=>
