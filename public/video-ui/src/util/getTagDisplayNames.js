902183cf9634f17a2a0676a0995f8d2853604f59
Michael Barton <michael.barton@guardian.co.uk>
2019-05-31 14:35:35 +0100

    Use internalName as the display name for tags
    I've retained the original logic so we are not tightly coupled to CAPI retaining the field. I can't imagine it's going to disappear but this at least means we don't break if it does.

--- public/video-ui/src/util/getTagDisplayNames.js
+++ public/video-ui/src/util/getTagDisplayNames.js
@@ -1,32 +1,38 @@
 import TagTypes from '../constants/TagTypes';
 
+// Logic from before CAPI returned internal names for tags
+function getLegacyDetailedTitle(tag) {
+  const tagType = tag.type;
+
+  if (tagType === TagTypes.keyword) {
+    //Some webtitles on keyword tags are too unspecific and we need to add
+    //the section name to them to know what tags they are referring to
+    return tag.webTitle !== tag.sectionName
+      ? `${tag.webTitle} (${tag.sectionName})`
+      : tag.webTitle;
+  } else {
+    const appendTagTypes = [
+      TagTypes.series,
+      TagTypes.tone,
+      TagTypes.commercial
+    ];
+
+    return appendTagTypes.includes(tagType)
+      ? `${tag.webTitle} (${tagType})`
+      : tag.webTitle;
+  }
+}
+
 export default function getTagDisplayNames(tags) {
   return tags.map(tag => {
     if (typeof tag === 'string') {
       return tag;
     }
 
-    const tagType = tag.type;
-    const tagForDisplay = { id: tag.id, webTitle: tag.webTitle };
-
-    if (tagType === TagTypes.keyword) {
-      //Some webtitles on keyword tags are too unspecific and we need to add
-      //the section name to them to know what tags they are referring to
-      tagForDisplay.detailedTitle = tag.webTitle !== tag.sectionName
-        ? `${tag.webTitle} (${tag.sectionName})`
-        : tag.webTitle;
-    } else {
-      const appendTagTypes = [
-        TagTypes.series,
-        TagTypes.tone,
-        TagTypes.commercial
-      ];
-
-      tagForDisplay.detailedTitle = appendTagTypes.includes(tagType)
-        ? `${tag.webTitle} (${tagType})`
-        : tag.webTitle;
-    }
-
-    return tagForDisplay;
+    return {
+      id: tag.id,
+      webTitle: tag.webTitle,
+      detailedTitle: tag.internalName ? tag.internalName : getLegacyDetailedTitle(tag)
+    };
   });
 }
