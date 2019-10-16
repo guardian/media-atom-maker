cb73743b18402b73a1ae359f1e524061b3fd820c
Martin Kuzdowicz <martin.kuzdowicz@gmail.com>
2019-10-16 17:49:02 +0100

    adding comment on why we are doing this

--- common/src/main/scala/com/gu/media/youtube/YoutubeDescription.scala
+++ common/src/main/scala/com/gu/media/youtube/YoutubeDescription.scala
@@ -18,6 +18,13 @@ object YoutubeDescription {
       html.text().replace("\\n", "\n")
     })
 
+    /**
+     * W edo this additional check if Option is defined and if ti contain empty string
+     * Ignore it and return None
+     * because if we will return Some("") we will have problems with putting that data into DynamoDB
+     * as DynamoDB does not allow empty string and will throw exception:
+     * Dynamo was unable to process this request. Error message One or more parameter values were invalid: An AttributeValue may not contain an empty string
+     */
     if (desc.isDefined && desc.get.isEmpty) return None
     desc
   }
