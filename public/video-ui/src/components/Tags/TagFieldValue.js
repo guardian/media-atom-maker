7c76c92a1e97607fd5d8c6cd2d6b98721153acb1
Stephen Geller <stephen.gb.geller+git@gmail.com>
2020-08-07 14:26:17 +0100

    Don't render object, render either webTitle or ID

--- public/video-ui/src/components/Tags/TagFieldValue.js
+++ public/video-ui/src/components/Tags/TagFieldValue.js
@@ -1,31 +1,26 @@
 import React from 'react';
 
 export default class TagFieldValue extends React.Component {
-
   renderFieldValue(value, index) {
+    console.log(value);
     if (value.detailedTitle) {
       return (
         <span key={`${value.id}-${index}`}>
-          <span className="form__field__tag__display">{value.detailedTitle}</span>
-          {' '}
+          <span className="form__field__tag__display">
+            {value.detailedTitle}
+          </span>{' '}
         </span>
       );
-
     }
 
     if (index === 0 || value === ',') {
-      return value;
+      return value.webTitle ?? value.id;
     }
 
-    return ` ${value}`;
+    return ` ${value.webTitle ?? value.id}`;
   }
 
   render() {
-    return (
-      <span>
-        {this.props.tagValue.map(this.renderFieldValue)}
-      </span>
-    );
-
+    return <span>{this.props.tagValue.map(this.renderFieldValue)}</span>;
   }
 }
