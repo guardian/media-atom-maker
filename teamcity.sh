6b8f078362a4cf3eb3ae8e6ce717fb2e57e6767d
Akash Askoolum <akash1810@gmail.com>
2021-03-10 11:55:29 +0000

    chore(ci): updating the CI script to observe the node versions defined in .nvmrc
    Not sure why there are different versions of node being used. That can be addressed later!
    
    Co-authored-by: TJ Silver <thalia.silver@guardian.co.uk>

--- teamcity.sh
+++ teamcity.sh
@@ -2,37 +2,51 @@
 
 set -e
 
-install_yarn() {
-  npm install -g yarn
+setupNvm() {
+  export NVM_DIR="$HOME/.nvm"
+  if [ -s "$NVM_DIR/nvm.sh" ]; then
+    # This loads nvm if installed directly
+    source "$NVM_DIR/nvm.sh"
+  elif [ -s "$(brew --prefix nvm)/nvm.sh" ]; then
+    # This will load nvm if installed via brew
+    source "$(brew --prefix nvm)/nvm.sh"
+  else
+    echo "Can't find NVM"
+    exit 1
+  fi
 }
 
-js_pluto_lambda(){
+buildPlutoLambda(){
   pushd pluto-message-ingestion
-  yarn install
-  yarn run build
+
+  # The pluto lambda uses a different version of node from main app.
+  # Change node versions and install yarn
+  # TODO use the same node version throughout the repo
+  nvm install
+  nvm use
+  npm install -g yarn
+
+  yarn
+  yarn build
   popd
 }
 
-js_deps() {
-  yarn install --force --non-interactive
-}
+buildApp() {
+  nvm install
+  nvm use
+  npm install -g yarn
 
-js_test() {
+  yarn install --force --non-interactive
   yarn lint
   yarn test
-}
-
-js_build() {
   yarn run build-autotrack
   yarn run build
 }
 
 main() {
-  install_yarn
-  js_pluto_lambda
-  js_deps
-  js_test
-  js_build
+  setupNvm
+  buildPlutoLambda
+  buildApp
 }
 
 main
