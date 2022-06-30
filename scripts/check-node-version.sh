7e2d62d935c0a7d892c1002e99e617b1498d70fc
Rhys Mills <rhystmills@gmail.com>
2022-06-30 10:00:23 +0100

    Limit range of red text colour

--- scripts/check-node-version.sh
+++ scripts/check-node-version.sh
@@ -5,6 +5,7 @@ set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 red='\x1B[0;31m'
+plain='\033[0m'
 
 checkNodeMatchesNvm() {
     NODE_MAJOR_VERSION=$(node -v | cut -d "." -f 1)
@@ -12,9 +13,9 @@ checkNodeMatchesNvm() {
 
     if [[ "v${DESIRED_NODE_VERSION}" != "${NODE_MAJOR_VERSION}"* ]]; then
         echo -e "${red}Your node version ${NODE_MAJOR_VERSION} does not match the version the project expects (v${DESIRED_NODE_VERSION})."
-        echo -e "${red}Please ensure it matches before starting the project."
-        echo -e "${red}If you are using nvm, for example, run \`nvm use\`."
-        echo -e "${red}https://github.com/creationix/nvm"
+        echo -e "Please ensure it matches before starting the project."
+        echo -e "If you are using nvm, for example, run \`nvm use\`."
+        echo -e "https://github.com/creationix/nvm${plain}"
         exit 1
     fi
 }
