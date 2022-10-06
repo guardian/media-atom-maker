de4bd397ff131bdadad7e1bac20c6eef0b2b190e
Andrew Nowak <andrew.nowak@guardian.co.uk>
2022-10-06 14:18:44 +0100

    automatically kill webpack once start script finishes

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,3 +7,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 "${DIR}/check-node-version.sh"
 yarn run build-dev &
 ./sbt $@ app/run
+
+# catches script exit events and kills the `yarn run build-dev` process and its children
+# https://stackoverflow.com/a/22644006
+trap "exit" INT TERM
+trap "kill 0" EXIT
