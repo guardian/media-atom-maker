f56ecdc0d20665a4b07bf9c2695d7b5366a97e1d
Andrew Nowak <andrew.nowak@guardian.co.uk>
2022-10-10 17:13:18 +0100

    replace committed sbt script/jar

--- scripts/client-dev.sh
+++ scripts/client-dev.sh
@@ -23,4 +23,4 @@ rm public/video-ui/build/main.css 2> /dev/null
 printf "\n\rStarting Webpack Dev Server... \n\r\n\r"
 yarn run client-dev &
 printf "\n\rStarting Play App... \n\r\n\r"
-JS_ASSET_HOST=https://video-assets.local.dev-gutools.co.uk/assets/ ./sbt $@ app/run
+JS_ASSET_HOST=https://video-assets.local.dev-gutools.co.uk/assets/ sbt $@ app/run
