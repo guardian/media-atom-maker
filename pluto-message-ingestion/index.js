fb4816c60c752a7553e33d59c759c89466d70631
Rhys Mills <rhys_mills@31752.gnm.int>
2023-05-26 10:02:54 +0100

    Add temp log lines

--- pluto-message-ingestion/index.js
+++ pluto-message-ingestion/index.js
@@ -8,13 +8,17 @@ exports.handler = (event, context, callback) => {
   kinesisMessageProcessor
     .open()
     .then(() => {
+      // eslint-disable-next-line no-console
+      console.log({ records: event.Records, count: event.Records.length });
       event.Records.forEach(record => {
         // Kinesis data is base64 encoded so decode here
         const payload = new Buffer(record.kinesis.data, 'base64').toString(
           'utf8'
         );
-        console.log({record})
-        console.log({payload})
+        // eslint-disable-next-line no-console
+        console.log({ record });
+        // eslint-disable-next-line no-console
+        console.log({ payload });
         try {
           const jsonPayload = JSON.parse(payload);
           kinesisMessageProcessor.process(jsonPayload);
