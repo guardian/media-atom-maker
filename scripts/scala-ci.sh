9db3c38f89877f3c6c9ced57a3b56abc99d7e720
Rhys Mills <rhys.mills@guardian.co.uk>
2023-06-23 14:11:52 +0100

    Use node 14 for build steps

--- scripts/scala-ci.sh
+++ scripts/scala-ci.sh
@@ -2,6 +2,6 @@
 
 set -e
  # Ensure we don't overwrite existing (Teamcity) builds.
-  LAST_TEAMCITY_BUILD=1280
+  LAST_TEAMCITY_BUILD=4800
   export GITHUB_RUN_NUMBER=$(( $GITHUB_RUN_NUMBER + $LAST_TEAMCITY_BUILD ))
   sbt clean compile test riffRaffUpload
