ea44cc79bc473b22d4461b5fdd55b5555b42d380
Andrew Nowak <andrew.nowak@guardian.co.uk>
2024-07-09 16:11:50 +0100

    Add unauthed message

--- app/controllers/PanDomainAuthActions.scala
+++ app/controllers/PanDomainAuthActions.scala
@@ -5,10 +5,15 @@ import com.gu.media.logging.Logging
 import com.gu.pandahmac.HMACAuthActions
 import com.gu.pandomainauth.model.AuthenticatedUser
 import play.api.Configuration
+import play.api.mvc.{RequestHeader, Result}
+import play.api.mvc.Results.Forbidden
 
 trait PanDomainAuthActions extends HMACAuthActions with Logging {
 
   def conf: Configuration
+
+  private def noPermissionMessage(authedUser: AuthenticatedUser): String =
+    s"user ${authedUser.user.email} does not have ${Permissions.basicAccess.name} permission"
 
   override def validateUser(authedUser: AuthenticatedUser): Boolean = {
     val isValid =
@@ -20,11 +25,22 @@ trait PanDomainAuthActions extends HMACAuthActions with Logging {
     if (!isValid) {
       log.warn(s"User ${authedUser.user.email} is not valid")
     } else if (!hasBasicAccess) {
-      log.warn(s"User ${authedUser.user.email} does not have media_atom_maker_access permission but does have other MAM permissions")
+      log.warn(noPermissionMessage(authedUser))
     }
 
     isValid && hasBasicAccess
   }
+
+  override def showUnauthedMessage(message: String)(implicit request: RequestHeader): Result =
+    Forbidden(views.html.authError(message))
+
+  override def invalidUserMessage(authedUser: AuthenticatedUser) = {
+    val hasBasicAccess = permissionsProvider.hasPermission(Permissions.basicAccess, authedUser.user)
+
+    if (!hasBasicAccess) noPermissionMessage(authedUser)
+    else super.invalidUserMessage(authedUser)
+  }
+
 
   override def authCallbackUrl: String = "https://" + conf.get[String]("host") + "/oauthCallback"
 
