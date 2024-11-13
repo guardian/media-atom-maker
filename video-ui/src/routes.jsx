f7722a00dfa9050f342a9cbd8b51750c5261da66
Rhys Mills <rhys.mills@guardian.co.uk>
2024-11-13 14:20:30 +0000

    Get vite build working (other than fonts). Webpack deps need to be removed, and so did reqwest, in order to get the app building properly. Panda requests now use 'fetch' instead. The Search page was accidentally removed in an earlier commit, this reinstates it.

--- video-ui/src/routes.jsx
+++ video-ui/src/routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Router, Route, browserHistory, IndexRedirect, Redirect } from 'react-router';
 
+import Search from './pages/Search';
 import Video from './pages/Video';
 import Upload from './pages/Upload/';
 import Help from './pages/Help';
@@ -11,6 +12,7 @@ export const routes = (
   <Router history={browserHistory}>
     <Route path="/" component={ReactApp}>
       <IndexRedirect to="/videos" />
+      <Route path="/videos" component={Search} />
       <Redirect from="/videos/create" to="/create" />
       <Route path="/videos/:id" component={Video} />
       <Route path="/videos/:id/upload" component={Upload} />
