ce0c60708b664782c2f776ecb619cca8d5beeff1
David Furey <david.furey@guardian.co.uk>
2025-05-27 15:59:46 +0100

    Tidying up

--- common/src/main/scala/com/gu/media/aws/MediaConvertAccess.scala
+++ common/src/main/scala/com/gu/media/aws/MediaConvertAccess.scala
@@ -6,6 +6,8 @@ import com.gu.media.Settings
 trait MediaConvertAccess { this: Settings with AwsAccess =>
   //lazy val transcodePipelineId = getMandatoryString("aws.transcoder.pipelineId") // perhaps replace this concept with job tags?
 
+  lazy val mediaConvertRole = getMandatoryString("aws.mediaconvert.role")
+
   lazy val mediaConvertClient = AWSMediaConvertClientBuilder
     .standard()
     .withCredentials(credsProvider)
