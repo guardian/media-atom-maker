0ae44cf84f614daf12deae10b458ac86f9c0e186
David Furey <david.furey@guardian.co.uk>
2025-05-28 09:27:35 +0100

    Cloudform media convert role

--- common/src/main/scala/com/gu/media/aws/MediaConvertAccess.scala
+++ common/src/main/scala/com/gu/media/aws/MediaConvertAccess.scala
@@ -6,7 +6,7 @@ import com.gu.media.Settings
 trait MediaConvertAccess { this: Settings with AwsAccess =>
   //lazy val transcodePipelineId = getMandatoryString("aws.transcoder.pipelineId") // perhaps replace this concept with job tags?
 
-  lazy val mediaConvertRole = getMandatoryString("aws.mediaconvert.role")
+  lazy val mediaConvertRole = sys.env.getOrElse("MEDIA_CONVERT_ROLE", getMandatoryString("aws.mediaconvert.role"))
 
   lazy val destinationBucket = getMandatoryString("aws.mediaconvert.destinationBucket")
 
