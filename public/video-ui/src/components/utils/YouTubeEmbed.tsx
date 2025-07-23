430a1701208f92a4b45103be303eb9b47181bf95
Anna Beddow <anna.beddow@gmail.com>
2025-07-23 17:37:33 +0100

    convert youtube embed to tsx file and refactor

--- public/video-ui/src/components/utils/YouTubeEmbed.tsx
+++ public/video-ui/src/components/utils/YouTubeEmbed.tsx
@@ -2,20 +2,18 @@ import React from 'react';
 import { getStore } from '../../util/storeAccessor';
 
 
-export const getYouTubeEmbedUrl = (id) => {
+export const getYouTubeEmbedUrl = (id: string) => {
   const embedUrl = getStore().getState().config.youtubeEmbedUrl;
   return `${embedUrl}${id}?showinfo=0&rel=0`;
 }
-export function YouTubeEmbed({ id, className, largePreview }) {
+export function YouTubeEmbed({ id, className, largePreview } : {id: string, className?: string, largePreview?: boolean }) {
   return (
     <iframe
-      type="text/html"
       className={className}
       src={getYouTubeEmbedUrl(id)}
       allowFullScreen
-      frameBorder="0"
-      height={largePreview ? "250px" : undefined}
-      width={largePreview ? "400px" : undefined}
+      frameBorder={0}
+      {...(largePreview && { height: "250px", width: "400px" })}
     />
   );
 }
