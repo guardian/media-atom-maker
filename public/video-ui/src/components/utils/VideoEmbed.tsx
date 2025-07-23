fcd9814f9823d1dc187bd9f0eac15225d59f2774
Anna Beddow <anna.beddow@gmail.com>
2025-07-23 17:45:26 +0100

    convert video embed to tsx file and refactor optional video props object

--- public/video-ui/src/components/utils/VideoEmbed.tsx
+++ public/video-ui/src/components/utils/VideoEmbed.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
 
 export function VideoEmbed({ sources, posterUrl }) {
-  const props = {
+  const videoProps = {
     className: 'video-player',
     controls: 'controls',
-    preload: 'metadata'
+    preload: 'metadata',
+    ...(posterUrl && { poster: posterUrl}),
   };
 
-  if (posterUrl) {
-    props.poster = posterUrl;
-  }
 
   if (sources.length === 1) {
     // to appease Safari
-    return <video src={sources[0].src} {...props} />;
+    return <video src={sources[0].src} {...videoProps} />;
   } else {
     return (
-      <video {...props}>
+      <video {...videoProps}>
         {sources.map(source => {
           return (
             <source key={source.src} src={source.src} type={source.mimeType} />
