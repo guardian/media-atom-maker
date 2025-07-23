086a6ddbb58b948fb7f4a388f057aa71609a6ba2
Anna Beddow <anna.beddow@gmail.com>
2025-07-23 17:52:08 +0100

    formatting

--- public/video-ui/src/components/utils/YouTubeEmbed.tsx
+++ public/video-ui/src/components/utils/YouTubeEmbed.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { getStore } from '../../util/storeAccessor';
 
-
 export const getYouTubeEmbedUrl = (id: string) => {
   const embedUrl = getStore().getState().config.youtubeEmbedUrl;
   return `${embedUrl}${id}?showinfo=0&rel=0`;
-}
-export function YouTubeEmbed({ id, className, largePreview } : {id: string, className?: string, largePreview?: boolean }) {
+};
+export function YouTubeEmbed({
+  id,
+  className,
+  largePreview
+}: {
+  id: string;
+  className?: string;
+  largePreview?: boolean;
+}) {
   return (
     <iframe
       className={className}
       src={getYouTubeEmbedUrl(id)}
       allowFullScreen
       frameBorder={0}
-      {...(largePreview && { height: "250px", width: "400px" })}
+      {...(largePreview && { height: '250px', width: '400px' })}
     />
   );
 }
