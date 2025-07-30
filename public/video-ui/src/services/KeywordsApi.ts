801a6c401dce387200d8ba703472b167f5bdb127
David Blatcher <david.blatcher@guardian.co.uk>
2025-07-30 16:29:29 +0100

    add types to requests for video and keyword api

--- public/video-ui/src/services/KeywordsApi.ts
+++ public/video-ui/src/services/KeywordsApi.ts
@@ -3,7 +3,7 @@ import { pandaReqwest } from './pandaReqwest';
 export default {
   composerTagToYouTube: (tagId: string) => {
     const encodedId = encodeURIComponent(tagId);
-    return pandaReqwest({
+    return pandaReqwest<string[]>({
       url: '/api/youtube/content-bundle/' + encodedId,
       method: 'get'
     });
