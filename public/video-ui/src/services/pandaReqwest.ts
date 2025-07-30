ecff875bb0f4ae05dcd6fb0518907949c197a53f
David Blatcher <david.blatcher@guardian.co.uk>
2025-07-30 13:38:18 +0100

    convert pandaReqwest to ts

--- public/video-ui/src/services/pandaReqwest.ts
+++ public/video-ui/src/services/pandaReqwest.ts
@@ -1,25 +1,27 @@
 import { reEstablishSession } from 'panda-session';
 import { getStore } from '../util/storeAccessor';
 
-const checkStatus = (res) => {
+const checkStatus = (res: Response) => {
   if (res.status >= 200 && res.status < 300) {
     return res;
   } else {
     throw res;
   }
-}
+};
 
-export const poll = (url, body, timeout) => {
+
+
+const poll = <ResponseBodyType = any>(url: string | URL | Request, body: RequestInit, timeout: number) => {
   const endTime = Number(new Date()) + timeout;
   const interval = 100;
 
-  const makeRequest = (resolve, reject) => {
+  const makeRequest = (resolve: (value: ResponseBodyType) => void, reject: (reason?: any) => void) => {
     fetch(url, body)
       .then(checkStatus)
       .then(response => {
         const contentTypeHeader = response.headers.get("content-type");
 
-        if (contentTypeHeader && contentTypeHeader.includes("application/json")){
+        if (contentTypeHeader && contentTypeHeader.includes("application/json")) {
           return response.json();
         }
         return response.text();
@@ -33,12 +35,12 @@ export const poll = (url, body, timeout) => {
             const store = getStore();
             const reauthUrl = store.getState().config.reauthUrl;
             reEstablishSession(reauthUrl, 5000).then(
-                () => {
-                  setTimeout(makeRequest, interval, resolve, reject);
-                },
-                error => {
-                  throw error;
-                });
+              () => {
+                setTimeout(makeRequest, interval, resolve, reject);
+              },
+              error => {
+                throw error;
+              });
           } else {
             setTimeout(makeRequest, interval, resolve, reject);
           }
@@ -51,8 +53,19 @@ export const poll = (url, body, timeout) => {
   return new Promise(makeRequest);
 };
 
+// https://github.com/ded/reqwest?tab=readme-ov-file#options - library that project used to use?
+type ReqwestBody<RequestBodyType = any> = RequestInit & {
+  url: string | URL | Request, // to do - might always be string
+  headers?: Record<string, string>;
+  data?: RequestBodyType;
+  type?: 'html' | 'xml' | 'json' | 'jsonp' // to we 
+  contentType?: string; // define enum of acceptable values?
+  crossOrigin?: boolean
+  withCredentials?: boolean
+}
+
 // when `timeout` > 0, the request will be retried every 100ms until success or timeout
-export const pandaReqwest = (reqwestBody, timeout = 0) => {
+export const pandaReqwest = <ResponseBodyType = any, RequestBodyType = any >(reqwestBody: ReqwestBody<RequestBodyType>, timeout = 0) => {
   const payload = Object.assign({ method: 'get', credentials: 'include' }, reqwestBody);
 
   if (payload.data) {
@@ -60,7 +73,7 @@ export const pandaReqwest = (reqwestBody, timeout = 0) => {
 
     // prettier-ignore
     if (payload.contentType === 'application/json' && typeof payload.data === 'object') {
-      if (payload.headers){
+      if (payload.headers) {
         payload.headers["Content-Type"] = "application/json";
       } else {
         payload.headers = {
@@ -71,5 +84,5 @@ export const pandaReqwest = (reqwestBody, timeout = 0) => {
     }
   }
 
-  return poll(reqwestBody.url, payload, timeout);
-}
+  return poll<ResponseBodyType>(reqwestBody.url, payload, timeout);
+};
