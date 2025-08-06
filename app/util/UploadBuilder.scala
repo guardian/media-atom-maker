fde188c27415e2ccb6653c0cb3fcebc14e9740ce
Andy Noton <andy.noton.contractor@guardian.co.uk>
2025-08-06 11:43:49 +0100

    prevent transcoder error by removing premature addition of m3u8 output

--- app/util/UploadBuilder.scala
+++ app/util/UploadBuilder.scala
@@ -47,9 +47,7 @@ object UploadBuilder {
     } else {
       val mp4Key = TranscoderOutputKey(title, id, "mp4").toString
       val mp4Source = VideoSource(mp4Key, "video/mp4")
-      val m3u8Key = TranscoderOutputKey(title, id, "m3u8").toString
-      val m3u8Source = VideoSource(m3u8Key, "application/vnd.apple.mpegurl")
-      Some(SelfHostedAsset(List(mp4Source, m3u8Source)))
+      Some(SelfHostedAsset(List(mp4Source)))
     }
   }
 
