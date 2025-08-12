f4407cee2905be8b1403f893cfdec533a6cdf1d0
Sam Hession <samhession@live.co.uk>
2025-08-12 12:53:14 +0100

    Replace get channels and categories with slice async thunks

--- public/video-ui/src/slices/youtubeSlice.ts
+++ public/video-ui/src/slices/youtubeSlice.ts
@@ -1,20 +1,62 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import {getYoutubeCategories, getYoutubeChannels} from "../services/YoutubeApi";
+import Logger from "../logger";
 
 const initialState: { categories: unknown[], channels: unknown[] } = { categories: [], channels: [] };
+
+const errorReceivingCategories = (error: unknown) => {
+  return {
+    type: 'SHOW_ERROR',
+    message: 'Could not get YouTube categories',
+    error: JSON.stringify(error),
+    receivedAt: Date.now()
+  };
+};
+
+export const fetchCategories = createAsyncThunk(
+  'youtube/fetchCategories',
+  async (_, {dispatch}) => {
+    return await getYoutubeCategories().catch(error => {
+        dispatch(errorReceivingCategories(error));
+      throw error;
+      }
+    );
+  }
+);
+
+const errorReceivingChannels = (error: unknown) => {
+  return {
+    type: 'SHOW_ERROR',
+    message: 'Could not get YouTube channels',
+    error: JSON.stringify(error),
+    receivedAt: Date.now()
+  };
+};
+
+export const fetchChannels = createAsyncThunk(
+  'youtube/fetchChannels',
+  async (_, {dispatch}) => {
+    return await getYoutubeChannels().catch(error => {
+        dispatch(errorReceivingChannels(error));
+        throw error;
+      }
+    );
+  }
+);
 
 const youtubeSlice = createSlice({
   name: 'youtube',
   initialState,
-  reducers: {
-    categoriesReceived: (state, action) => {
-      state.categories = action.payload;
-    },
-    channelsReceived: (state, action) => {
-      state.channels = action.payload;
-    }
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCategories.fulfilled, (state, action) => {
+        state.categories = action.payload as any;
+      })
+      .addCase(fetchChannels.fulfilled, (state, action) => {
+        state.channels = action.payload as any;
+      });
   }
 });
-
-export const { categoriesReceived, channelsReceived } = youtubeSlice.actions;
 
 export default youtubeSlice.reducer;
