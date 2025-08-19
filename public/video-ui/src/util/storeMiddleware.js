dd49398a0c7b3ad48927310ae75b7f7b8cdcee4f
Sam Hession <samhession@live.co.uk>
2025-08-19 10:52:19 +0100

    Adds error redux toolkit slice and removes old reducer

--- public/video-ui/src/util/storeMiddleware.js
+++ public/video-ui/src/util/storeMiddleware.js
@@ -1,34 +1,35 @@
+import { clearError } from '../slices/error';
 import { updatePath } from "../slices/path";
 
-export const storeMiddleware = ({ dispatch, getState }) => next => action => {
-  const prevState = getState();
-  next(action);
-  getState();
+export const storeMiddleware =
+  ({ dispatch, getState }) =>
+  next =>
+  action => {
+    const prevState = getState();
+    next(action);
+    getState();
 
-  if (prevState.path !== location.pathname) {
-    dispatch(
-      updatePath(location.pathname)
-    );
+    if (prevState.path !== location.pathname) {
+      dispatch(
+        updatePath(location.pathname)
+      );
 
-    dispatch({
-      type: 'VIDEO_POPULATE_BLANK',
-      receivedAt: Date.now()
-    });
+      dispatch({
+        type: 'VIDEO_POPULATE_BLANK',
+        receivedAt: Date.now()
+      });
 
-    dispatch({
-      type: 'USAGE_UPDATE_BLANK',
-      receivedAt: Date.now()
-    });
+      dispatch({
+        type: 'USAGE_UPDATE_BLANK',
+        receivedAt: Date.now()
+      });
 
-    dispatch({
-      type: 'CLEAR_ERROR',
-      receivedAt: Date.now()
-    });
+      dispatch(clearError());
 
-    dispatch({
-      type: 'VIDEO_EDIT_STATE_REQUEST',
-      state: false,
-      receivedAt: Date.now()
-    });
-  }
-};
+      dispatch({
+        type: 'VIDEO_EDIT_STATE_REQUEST',
+        state: false,
+        receivedAt: Date.now()
+      });
+    }
+  };
