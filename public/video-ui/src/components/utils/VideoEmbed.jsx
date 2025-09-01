f55361d3be3390556a109eeae468e2764508487b
Andy Noton <andy.noton.contractor@guardian.co.uk>
2025-09-01 17:14:19 +0100

    improve readability

--- public/video-ui/src/components/utils/VideoEmbed.jsx
+++ public/video-ui/src/components/utils/VideoEmbed.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
 
-function getVttName(m3u8Src) {
-  // captions suffix is an assumption based on MediaConvert output we've seen
-  return m3u8Src.replace(/.m3u8$/, "captions_00001.vtt");
-}
-
 export function VideoEmbed({ sources, posterUrl }) {
   const props = {
     className: 'video-player',
@@ -20,13 +15,8 @@ export function VideoEmbed({ sources, posterUrl }) {
     // to appease Safari
     return <video src={sources[0].src} {...props} />;
   } else {
-    // if m3u8 and mp4 are both present, put m3u8 first and add subtitle track for browsers that don't support m3u8
-    const m3u8 = sources.find(source => source.mimeType === "application/vnd.apple.mpegurl");
-    const mp4 = sources.find(source => source.mimeType === "video/mp4");
-    const sortedSources =
-      (m3u8 && mp4) ? [m3u8].concat(sources.filter(source => source !== m3u8)) : sources;
-    const subtitleTrack =
-      (m3u8 && mp4) ? <track default kind="subtitles" src={getVttName(m3u8.src)}/> : undefined;
+    const {subtitleTrack, sortedSources} = includeSubtitles(sources);
+
     if (subtitleTrack) {
       // need to use CORS to load subtitle track
       props.crossOrigin = "anonymous";
@@ -43,4 +33,20 @@ export function VideoEmbed({ sources, posterUrl }) {
       </video>
     );
   }
+}
+
+function includeSubtitles(sources) {
+  // if m3u8 and mp4 are both present, put m3u8 first and add subtitle track for browsers that don't support m3u8
+  const m3u8 = sources.find(source => source.mimeType === "application/vnd.apple.mpegurl");
+  const mp4 = sources.find(source => source.mimeType === "video/mp4");
+  const sortedSources =
+    (m3u8 && mp4) ? [m3u8].concat(sources.filter(source => source !== m3u8)) : sources;
+  const subtitleTrack =
+    (m3u8 && mp4) ? <track default kind="subtitles" src={getVttName(m3u8.src)}/> : undefined;
+  return {subtitleTrack, sortedSources};
+}
+
+function getVttName(m3u8Src) {
+  // captions suffix is an assumption based on MediaConvert output we've seen
+  return m3u8Src.replace(/.m3u8$/, "captions_00001.vtt");
 }
