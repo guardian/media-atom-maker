cbc319741a932be564cc122823352f6ce3d0fc89
fweddi <freddiepreece@outlook.com>
2025-09-09 16:22:32 +0100

    add asset type serialisation

--- public/video-ui/src/services/VideosApi.ts
+++ public/video-ui/src/services/VideosApi.ts
@@ -7,7 +7,7 @@ import { ContentAtom, getVideoBlock } from '../util/getVideoBlock';
 
 export type ComposerStage = 'live' | 'preview'
 
-export type AssetType = 'Audio' | 'Video'
+export type AssetType = 'Audio' | 'Video' | 'Subtitles'
 
 export type Platform = 'Youtube' | 'Facebook' | 'Dailymotion' | 'Mainstream' | 'Url'
 
