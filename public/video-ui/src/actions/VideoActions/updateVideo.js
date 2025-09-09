ddfbd2d1eb89eac65a9f64c06cc0cba21e3cd923
Sam Hession <samhession@live.co.uk>
2025-09-09 17:09:44 +0100

    Move some actions into new "setVideo" action

--- public/video-ui/src/actions/VideoActions/updateVideo.js
+++
@@ -1,8 +0,0 @@
-//NOTE: THIS DOESN'T SAVE THE VIDEO, ONLY UPDATES THE CLIENT STATE. USE saveVideo TO SAVE
-export function updateVideo(video) {
-  return {
-    type: 'VIDEO_UPDATE_REQUEST',
-    video: video,
-    receivedAt: Date.now()
-  };
-}
