aac1f9223da939faf432f421aed9fdac8ea02b09
Sam Hession <samhession@live.co.uk>
2025-09-10 08:57:59 +0100

    Turn set asset and set active asset into video slice actions

--- public/video-ui/src/actions/VideoActions/deleteAsset.js
+++ public/video-ui/src/actions/VideoActions/deleteAsset.js
@@ -1,20 +1,12 @@
 import VideosApi from '../../services/VideosApi';
 import { getUploads } from "../UploadActions/getUploads";
 import { setVideo } from "../../slices/video";
+import { showError } from "../../slices/error";
 
 function requestAssetDelete(assetId) {
   return {
     type: 'ASSET_DELETE_REQUEST',
     assetId,
-    receivedAt: Date.now()
-  };
-}
-
-function errorAssetDelete(error) {
-  return {
-    type: 'SHOW_ERROR',
-    message: 'Could not delete asset',
-    error: error,
     receivedAt: Date.now()
   };
 }
@@ -32,7 +24,7 @@ export function deleteAsset(video, assetId) {
         // Pull down the latest changes from the server because uploads and assets are managed differently
         dispatch(getUploads(video.id));
       })
-      .catch(err => dispatch(errorAssetDelete(err)));
+      .catch(err => dispatch(showError('Could not delete asset', err)));
   };
 }
 
@@ -51,6 +43,6 @@ export function deleteAssets(video, assetIds) {
         // Pull down the latest changes from the server because uploads and assets are managed differently
         dispatch(getUploads(video.id));
       })
-      .catch(err => dispatch(errorAssetDelete(err)));
+      .catch(err => dispatch(showError('Could not delete asset', err)));
   };
 }
