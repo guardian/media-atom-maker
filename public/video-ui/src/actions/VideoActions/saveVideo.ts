c3f63d833f6918d2e7fcc58539fd13dc0cf591e0
DBlatcher <david.blatcher@guardian.co.uk>
2025-09-18 14:19:56 +0100

    convert saveVideo to typescript, allow for thunk returning a rejected action

--- public/video-ui/src/actions/VideoActions/saveVideo.ts
+++ public/video-ui/src/actions/VideoActions/saveVideo.ts
@@ -1,19 +1,27 @@
-import VideosApi from '../../services/VideosApi';
+import VideosApi, { Video } from '../../services/VideosApi';
 import { showError } from '../../slices/error';
-import { fetchUsages, updateVideoUsageWebTitle } from '../../slices/usage';
+import { fetchUsages, updateVideoUsageWebTitle, UsageState } from '../../slices/usage';
 import { setSaving } from '../../slices/saveState';
 import { setVideo } from '../../slices/video';
+import { Action, ThunkDispatch } from '@reduxjs/toolkit';
+import { RootState } from '../../util/setupStore';
 
 
-export function saveVideo(video) {
-  return dispatch => {
+
+export function saveVideo(video: Video) {
+  return (dispatch: ThunkDispatch<RootState, void, Action>) => {
     dispatch(setSaving(true));
     dispatch(setVideo(video));
     return VideosApi.saveVideo(video.id, video)
       .then(res => {
         return dispatch(fetchUsages(video.id))
-          .then(fetchUsagesFulfilledAction => {
-            return VideosApi.updateCanonicalPages(video, fetchUsagesFulfilledAction.payload, 'preview');
+          .then(fetchUsagesAction => {
+            console.log(fetchUsagesAction);
+            if (fetchUsagesAction.meta.requestStatus === 'rejected') {
+              throw new Error('fetchUsagesFulfilledAction rejected');
+            }
+
+            return VideosApi.updateCanonicalPages(video, fetchUsagesAction.payload as UsageState, 'preview');
           })
           .then(() => {
             dispatch(updateVideoUsageWebTitle(video.title));
