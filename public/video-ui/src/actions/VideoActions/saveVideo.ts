7be1e9455b0575123077687dc5adfbfcf2bcee92
DBlatcher <david.blatcher@guardian.co.uk>
2025-09-22 13:23:56 +0100

    move the error messages out of the logic

--- public/video-ui/src/actions/VideoActions/saveVideo.ts
+++ public/video-ui/src/actions/VideoActions/saveVideo.ts
@@ -5,6 +5,12 @@ import { fetchUsages, updateVideoUsageWebTitle, UsageData } from '../../slices/u
 import { setVideo } from '../../slices/video';
 import { AppDispatch } from '../../util/setupStore';
 
+const errorMessages = {
+  saveVideoDefault: 'Could not save video',
+  saveVideoConflict: 'Could not save video as another user (or tab) is currently editing this video',
+  fetchUsagesRejected: 'Could not fetch usages to update canonical page',
+  canonicalPageUpdate: 'Could not update canonical page'
+};
 
 export const saveVideo = (video: Video) => async (dispatch: AppDispatch) => {
 
@@ -13,10 +19,9 @@ export const saveVideo = (video: Video) => async (dispatch: AppDispatch) => {
 
   const savedVideo: Video | undefined = await VideosApi.saveVideo(video.id, video)
     .catch(error => {
-      const defaultError = 'Could not save video';
-      const conflictError =
-        'Could not save video as another user (or tab) is currently editing this video';
-      const message = error.status === 409 ? conflictError : defaultError;
+      const message = error.status === 409
+        ? errorMessages.saveVideoConflict
+        : errorMessages.saveVideoDefault;
       dispatch(showError(message, error));
       return undefined;
     });
@@ -27,18 +32,17 @@ export const saveVideo = (video: Video) => async (dispatch: AppDispatch) => {
 
   const fetchUsagesAction = await dispatch(fetchUsages(video.id));
   if (fetchUsagesAction.meta.requestStatus !== 'fulfilled') {
-    dispatch(showError('Could not fetch usages to update canonical page', new Error(fetchUsagesAction.meta.requestStatus)));
+    dispatch(showError(errorMessages.fetchUsagesRejected, new Error(errorMessages.fetchUsagesRejected)));
     dispatch(setSaving(false));
     return;
   }
-
 
   const canonicalPageUpdate: unknown[] | undefined = await VideosApi.updateCanonicalPages(
     video, fetchUsagesAction.payload as UsageData,
     'preview'
   )
     .catch(error => {
-      dispatch(showError('Could not update canonical page', error));
+      dispatch(showError(errorMessages.canonicalPageUpdate, error));
       return undefined;
     });
   if (!canonicalPageUpdate) {
