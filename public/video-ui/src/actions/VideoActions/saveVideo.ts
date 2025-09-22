843d800891626403d0c8e8a871cdc44bfc00789a
DBlatcher <david.blatcher@guardian.co.uk>
2025-09-22 12:45:26 +0100

    refactor saveVideo function as async and show more informative error if fetchUsages rejects

--- public/video-ui/src/actions/VideoActions/saveVideo.ts
+++ public/video-ui/src/actions/VideoActions/saveVideo.ts
@@ -6,37 +6,47 @@ import { setVideo } from '../../slices/video';
 import { AppDispatch } from '../../util/setupStore';
 
 
+export const saveVideo = (video: Video) => async (dispatch: AppDispatch) => {
 
-export function saveVideo(video: Video) {
-  return (dispatch: AppDispatch) => {
-    dispatch(setSaving(true));
-    dispatch(setVideo(video));
-    return VideosApi.saveVideo(video.id, video)
-      .then(res => {
-        return dispatch(fetchUsages(video.id))
-          .then(fetchUsagesAction => {
-            if (fetchUsagesAction.meta.requestStatus === 'rejected') {
-              throw new Error('fetchUsagesFulfilledAction rejected');
-            }
-            return VideosApi.updateCanonicalPages(video, fetchUsagesAction.payload as UsageData, 'preview');
-          })
-          .then(() => {
-            dispatch(updateVideoUsageWebTitle(video.title));
-          })
-          .catch(error => () => {
-            dispatch(showError('Could not update canonical page', error));
-          });
-      })
-      .catch(error => {
-        const defaultError = 'Could not save video';
-        const conflictError =
-          'Could not save video as another user (or tab) is currently editing this video';
-        const message = error.status === 409 ? conflictError : defaultError;
-        dispatch(showError(message, error));
-        throw error;
-      })
-      .finally(() => {
-        dispatch(setSaving(false));
-      });
-  };
-}
+  dispatch(setSaving(true));
+  dispatch(setVideo(video));
+
+  const savedVideo: Video | undefined = await VideosApi.saveVideo(video.id, video)
+    .catch(error => {
+      const defaultError = 'Could not save video';
+      const conflictError =
+        'Could not save video as another user (or tab) is currently editing this video';
+      const message = error.status === 409 ? conflictError : defaultError;
+      dispatch(showError(message, error));
+      return undefined;
+    });
+  if (!savedVideo) {
+    dispatch(setSaving(false));
+    return;
+  }
+
+  const fetchUsagesAction = await dispatch(fetchUsages(video.id));
+  if (fetchUsagesAction.meta.requestStatus !== 'fulfilled') {
+    dispatch(showError('Could not fetch usages to update canonical page', new Error(fetchUsagesAction.meta.requestStatus)));
+    dispatch(setSaving(false));
+    return;
+  }
+
+
+  const canonicalPageUpdate: unknown[] | undefined = await VideosApi.updateCanonicalPages(
+    video, fetchUsagesAction.payload as UsageData,
+    'preview'
+  )
+    .catch(error => {
+      dispatch(showError('Could not update canonical page', error));
+      return undefined;
+    });
+  if (!canonicalPageUpdate) {
+    dispatch(setSaving(false));
+    return;
+  }
+
+  dispatch(updateVideoUsageWebTitle(video.title));
+  dispatch(setSaving(false));
+};
+
