efb801aa0ed51f7069222d034f7a556386985438
Sam Hession <samhession@live.co.uk>
2025-10-01 12:01:47 +0100

    Moves trackInWorkflow thunk into workflow slice

--- public/video-ui/src/pages/Video/index.jsx
+++ public/video-ui/src/pages/Video/index.jsx
@@ -416,9 +416,7 @@ import * as getPublishedVideo
   from '../../actions/VideoActions/getPublishedVideo';
 import * as videoPageUpdate
   from '../../actions/VideoActions/videoPageUpdate';
-import { getStatus } from '../../slices/workflow';
-import * as trackInWorkflow
-  from '../../actions/WorkflowActions/trackInWorkflow';
+import {getStatus, trackInWorkflow} from '../../slices/workflow';
 import * as updateWorkflowData
   from '../../actions/WorkflowActions/updateWorkflowData';
 import {getYouTubeEmbedUrl} from "../../components/utils/YouTubeEmbed";
@@ -461,7 +459,7 @@ function mapDispatchToProps(dispatch) {
       dispatch
     ),
     workflowActions: bindActionCreators(
-      { getStatus, ...trackInWorkflow, ...updateWorkflowData },
+      { getStatus, trackInWorkflow, ...updateWorkflowData },
       dispatch
     )
   };
