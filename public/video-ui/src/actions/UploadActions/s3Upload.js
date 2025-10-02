e0dac45667de711f1f1091b52cc3d53448bb8e0b
Sam Hession <samhession@live.co.uk>
2025-10-02 10:49:15 +0100

    Migrate S3Uploads startVideoUpload action to RTK style

--- public/video-ui/src/actions/UploadActions/s3Upload.js
+++ public/video-ui/src/actions/UploadActions/s3Upload.js
@@ -1,26 +1,7 @@
-import { createUpload, uploadParts, uploadPacFile, uploadSubtitleFile, deleteSubtitleFile} from '../../services/UploadsApi';
+import { uploadPacFile, uploadSubtitleFile, deleteSubtitleFile} from '../../services/UploadsApi';
 import { errorDetails } from '../../util/errorDetails';
 import {setS3UploadStatusToComplete, s3UploadProgress, setS3UploadStatusToError, s3UploadStarted} from "../../slices/s3Upload";
 import {showError} from "../../slices/error";
-
-export function startVideoUpload({id, file, selfHost}) {
-  return dispatch => {
-    createUpload(id, file, selfHost).then(upload => {
-      dispatch(s3UploadStarted(upload));
-
-      const progress = completed => dispatch(s3UploadProgress(completed));
-
-      return uploadParts(upload, upload.parts, file, progress)
-        .then(() => {
-          dispatch(setS3UploadStatusToComplete());
-        })
-        .catch(err => {
-          dispatch(showError(errorDetails(err), err));
-          dispatch(setS3UploadStatusToError());
-        });
-    });
-  };
-}
 
 export function startPacFileUpload({id, file}) {
   return dispatch => {
