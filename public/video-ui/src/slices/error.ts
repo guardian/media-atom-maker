73e4cfe26c8465d98d4594706284dd3e6fed0419
Sam Hession <samhession@live.co.uk>
2025-10-08 16:51:24 +0100

    Allows error slice to display a warning and an error

--- public/video-ui/src/slices/error.ts
+++ public/video-ui/src/slices/error.ts
@@ -27,13 +27,15 @@ export const clearError: () => Action<'CLEAR_ERROR'> = () => ({
 interface Error {
   message: false | string;
   key: number;
-  isWarning: boolean;
+  warningMessage: false | string;
+  warningKey: number;
 }
 
 const initialState: Error = {
   message: false,
   key: 0,
-  isWarning: false
+  warningMessage: false,
+  warningKey: 0
 };
 
 // Currently this slice users Extra Reducers to all for support of actions without
@@ -49,22 +51,25 @@ const error = createSlice({
       (state, { message }: ShowError) => {
         state.message = message;
         state.key++;
-        state.isWarning = false;
       }
     ),
     clearError: builder.addCase('CLEAR_ERROR', state => {
       state.message = false;
+      state.warningMessage = false;
     }),
-    showDurationWarning: builder.addCase(setActiveAsset, (state, { payload }) => {
-      const activeAsset = payload.assets.find(
-        a => a.version === payload.activeVersion
-      );
-      if (payload.duration === 0 && activeAsset?.platform === 'Youtube') {
-        state.message =
-          'YouTube reported the activated asset as being Live. If this is not the case you will need to manually set the duration.';
-        state.isWarning = true;
+    showDurationWarning: builder.addCase(
+      setActiveAsset,
+      (state, { payload }) => {
+        const activeAsset = payload.assets.find(
+          a => a.version === payload.activeVersion
+        );
+        if (payload.duration === 0 && activeAsset?.platform === 'Youtube') {
+          state.warningMessage =
+            'YouTube reported the activated asset as being Live. If this is not the case you will need to manually set the duration.';
+          state.warningKey++;
+        }
       }
-    })
+    )
   })
 });
 
