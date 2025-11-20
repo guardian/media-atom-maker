e9489ae9db8c72b5ec2c32aabfa227157c6332a7
fweddi <freddiepreece@outlook.com>
2025-11-20 17:08:03 +0000

    use correct name

--- public/video-ui/src/constants/videoPlayerFormats.ts
+++ public/video-ui/src/constants/videoPlayerFormats.ts
@@ -1,5 +1,5 @@
 export const videoPlayerFormats: {id: string, title: string}[] = [
-  {id: "Loop", title: "Loop (Auto-play looping video with controls)"},
-  {id: "Default", title: "Standard (Manual play video with controls)"},
-  {id: "Cinemagraph", title: "Cinemagraph (Auto-play looping animation without controls)"}
+  {id: "Loop", title: "Loop"},
+  {id: "Default", title: "Standard"},
+  {id: "Cinemagraph", title: "Cinemagraph"}
 ];
