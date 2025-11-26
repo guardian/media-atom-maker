0cb8ff083c958fb86865ac811d8da619f82dd767
fweddi <freddiepreece@outlook.com>
2025-11-26 15:53:09 +0000

    disable subtitles upload if cinemagraph or standard

--- public/video-ui/src/constants/videoPlayerFormats.ts
+++ public/video-ui/src/constants/videoPlayerFormats.ts
@@ -1,5 +1,9 @@
-export const videoPlayerFormats: {id: string, title: string}[] = [
+export const videoPlayerFormats: {id: VideoPlayerFormat, title: string}[] = [
   {id: "Loop", title: "Loop"},
   {id: "Default", title: "Standard"},
   {id: "Cinemagraph", title: "Cinemagraph"}
 ];
+
+export type VideoPlayerFormat = "Loop" | "Default" | "Cinemagraph";
+
+export type VideoPlayerOption = VideoPlayerFormat | 'Youtube';
