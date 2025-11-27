39dc6ad00cd78abfb2ec4380d7e0df3db23f4c39
lindseydew <lindsey.dew@guardian.co.uk>
2025-11-27 12:15:00 +0000

    Run formatter

--- common/src/main/scala/com/gu/media/ses/Mailer.scala
+++ common/src/main/scala/com/gu/media/ses/Mailer.scala
@@ -71,7 +71,8 @@ class Mailer(config: Settings with SESSettings) {
       subject: String,
       body: String
   ): SendEmailResponse = {
-    val subjectContent = Content.builder().data(s"[Media Atom Maker] $subject").build()
+    val subjectContent =
+      Content.builder().data(s"[Media Atom Maker] $subject").build()
     val bodyContent =
       Body.builder().html(Content.builder().data(body).build()).build()
     config.sesClient.sendEmail(
