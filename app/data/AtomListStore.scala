ccc842dfc844e90532b5b7996493e5784b184f9b
Andy Noton <andy.noton.contractor@guardian.co.uk>
2025-12-15 16:47:21 +0000

    fix json read of videoPlayerFormat

--- app/data/AtomListStore.scala
+++ app/data/AtomListStore.scala
@@ -121,7 +121,7 @@ class CapiBackedAtomListStore(capi: CapiAccess) extends AtomListStore {
         .orElse(activeAssetMediaPlatform)
 
       val videoPlayerFormat =
-        (atom \ "videoPlayerFormat").asOpt[VideoPlayerFormat]
+        (atom \ "metadata" \ "selfHost" \ "videoPlayerFormat").asOpt[VideoPlayerFormat]
 
       Some(
         MediaAtomSummary(
