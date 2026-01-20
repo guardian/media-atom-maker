290291796df1e55881c599b992a2a7f3976518d0
fweddi <freddiepreece@outlook.com>
2026-01-20 14:54:28 +0000

    Don't show modal contents until modal is open
    Since the introduction of the new dialog element (ec02d9bdc5d28b79c8969955e21031a2190f3b93), latent iframes from Grid can all be present on the DOM at the same time, even if they are not visible or interactable.
    
    This creates an issue with the video image fields, where the crop type in Grid is initially parsed from the url and then stored in session storage. The different iFrames each write their own crop type to the same field in session storage, and then attempt to read from that same field.
    
    This ultimately leads to the image picker enforcing incorrect crop sizes. This manifests in errors downstream. On Fronts, for example, we don't show an images if the incorrect crop has been applied.
    
    Due to the nature of the Dialog API, we can't hide the whole Modal component from the UI, but we can choose to conditionally render its contents. This means that only one Grid iFrame should be rendered at any one point in time, fixing the crop type conflict.
    
    Co-authored-by: Andrew Nowak <andrew.nowak@guardian.co.uk>

--- public/video-ui/src/components/utils/Modal.tsx
+++ public/video-ui/src/components/utils/Modal.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 
 export default class Modal extends React.Component {
   props: React.PropsWithChildren;
-  state: { dialogRef : React.RefObject<HTMLDialogElement> | undefined } = {
-    dialogRef: undefined
+  state: { dialogRef : React.RefObject<HTMLDialogElement> | undefined, isOpen: boolean } = {
+    dialogRef: undefined,
+    isOpen: false
   };
 
   showModal() {
     this.state.dialogRef.current?.showModal();
+    this.setState({ isOpen: true });
   }
 
   close() {
     this.state.dialogRef.current?.close();
+    this.setState({ isOpen: false });
   }
 
   constructor(props: React.PropsWithChildren) {
@@ -22,14 +25,16 @@ export default class Modal extends React.Component {
   render() {
     return (
       <dialog className="modal" ref={this.state.dialogRef} onClick={() => this.close()}>
-        <div
-          className="modal__content"
-          onClick={(e) => {
-            // Prevent clicks on modal content from closing the modal
-            e.stopPropagation();
-          }}>
-          {this.props.children}
-        </div>
+        {this.state.isOpen &&
+            <div
+                className="modal__content"
+                onClick={(e) => {
+                  // Prevent clicks on modal content from closing the modal
+                  e.stopPropagation();
+                }}>
+              {this.props.children}
+            </div>
+        }
         <div className="modal__content__header">
           <button
             className="i-cross button__secondary modal__dismiss"
