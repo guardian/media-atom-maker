718fc41b53581886121044ad7f7496476f9d963f
Andrew Nowak <andrew.nowak@guardian.co.uk>
2026-02-06 16:43:56 +0000

    Make a last minute check for existing composer pages before creating one

--- public/video-ui/src/actions/VideoActions/videoPageCreate.js
+++ public/video-ui/src/actions/VideoActions/videoPageCreate.js
@@ -12,9 +12,15 @@ function requestVideoPageCreate() {
 }
 
 export function createVideoPage(id, video, isTrainingMode) {
-  return dispatch => {
+  return async dispatch => {
     dispatch(requestVideoPageCreate());
 
+    // Do a last minute check to see if a video page has shown up when we weren't looking
+    const existingUsages = await VideosApi.getVideoUsages(id).then(res => res.preview.video.length > 0);
+
+    if (existingUsages) {
+      return dispatch(showError('Video page already exists, please refresh this page to get latest data'));
+    }
     return VideosApi.createComposerPage(id, video)
       .then(res => {
         const composerId = res.data.id;
