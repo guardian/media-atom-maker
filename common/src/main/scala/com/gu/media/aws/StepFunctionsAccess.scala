7a88088e522fc79ea65a098fc4a11a3f0ea87823
David Furey <david.furey@guardian.co.uk>
2026-02-24 17:37:10 +0000

    Enrich upload with dimensions in resume machine lambda
    This was done in GetTranscodingProgressV2 but we are about to remove that lambda since it is now redundant

---
+++ common/src/main/scala/com/gu/media/aws/StepFunctionsAccess.scala
@@ -0,0 +1,12 @@
+package com.gu.media.aws
+
+import com.gu.media.Settings
+import software.amazon.awssdk.services.sfn.SfnClient
+
+trait StepFunctionsAccess { this: Settings with AwsAccess =>
+  lazy val stepFunctionsClient = SfnClient
+    .builder()
+    .credentialsProvider(credentials.instance.awsV2Creds)
+    .region(awsV2Region)
+    .build()
+}
--- uploader/src/main/scala/com/gu/media/upload/ResumeMachine.scala
+++ uploader/src/main/scala/com/gu/media/upload/ResumeMachine.scala
@@ -1,15 +1,25 @@
 package com.gu.media.upload
 
-import com.gu.media.aws.MediaConvertAccess
+import com.gu.media.aws.{MediaConvertAccess, StepFunctionsAccess}
 import com.gu.media.lambda.LambdaWithParams
 import com.gu.media.logging.Logging
-import com.gu.media.upload.model.{MediaConvertEvent, WaitOnUpload}
-import play.api.libs.json.Json
-import software.amazon.awssdk.services.sfn.SfnClient
+import com.gu.media.model.{
+  ImageAssetDimensions,
+  SelfHostedAsset,
+  VideoAsset,
+  VideoSource
+}
+import com.gu.media.upload.model.{
+  MediaConvertEvent,
+  MediaConvertOutputGroupDetails,
+  WaitOnUpload
+}
+import play.api.libs.json.{Json, Reads}
 import software.amazon.awssdk.services.sfn.model.{
   GetExecutionHistoryRequest,
   HistoryEventType,
-  SendTaskSuccessRequest
+  SendTaskSuccessRequest,
+  SfnException
 }
 
 import scala.jdk.CollectionConverters._
@@ -17,41 +27,115 @@ import scala.jdk.CollectionConverters._
 class ResumeMachine
     extends LambdaWithParams[MediaConvertEvent, String]
     with MediaConvertAccess
+    with StepFunctionsAccess
     with Logging {
   override def handle(data: MediaConvertEvent): String = {
-    val stepFunctionsClient = SfnClient
-      .builder()
-      .credentialsProvider(credentials.instance.awsV2Creds)
-      .region(awsV2Region)
-      .build()
+    (for {
+      executionId <- data.detail.userMetadata
+        .get("executionId")
+        .toRight("User metadata did not contain executionId")
 
-    val executionId = data.detail.userMetadata.get("executionId").get
+      lastSateInput <- getLastStateInput[WaitOnUpload](executionId)
 
+      upload = lastSateInput.input
+
+      videoDimensions = getVideoDimensions(data.detail.outputGroupDetails)
+
+      updatedAsset = upload.metadata.asset.map(
+        applyDimensionsToAsset(_, videoDimensions)
+      )
+
+      output = upload.copy(
+        metadata = upload.metadata.copy(asset = updatedAsset),
+        progress = upload.progress.copy(retries = 0, fullyTranscoded = true)
+      )
+
+      sendTaskSuccessRequest = SendTaskSuccessRequest
+        .builder()
+        .taskToken(lastSateInput.taskToken)
+        .output(Json.stringify(Json.toJson(output)))
+        .build()
+
+      _ <- CatchAWSError(
+        stepFunctionsClient.sendTaskSuccess(sendTaskSuccessRequest)
+      )
+    } yield executionId) match {
+      case Left(error) => log.error(error)
+      case Right(executionId) =>
+        log.info(s"Resumed state machine execution $executionId")
+    }
+
+    "Done"
+  }
+
+  private def CatchAWSError[T](operation: => T): Either[String, T] = {
+    try {
+      Right(operation)
+    } catch {
+      case e: SfnException => Left(e.awsErrorDetails().errorMessage())
+    }
+  }
+
+  private def getLastStateInput[T](
+      executionId: String
+  )(implicit fjs: Reads[T]): Either[String, T] = {
     val historyRequest = GetExecutionHistoryRequest
       .builder()
       .executionArn(executionId)
       .reverseOrder(true)
       .build()
 
-    val historyResults =
-      stepFunctionsClient.getExecutionHistory(historyRequest).events().asScala
-    val lastScheduledEvent =
-      historyResults.find(_.`type` == HistoryEventType.TASK_SCHEDULED).head
-    val payload = Json.parse(
-      lastScheduledEvent.taskScheduledEventDetails().parameters()
-    ) \ "Payload"
-    val lastSateInput = payload.as[WaitOnUpload]
-
-    // todo: do something useful with `data`, the MediaConvertEvent rather than just resuming the step function
-    // probably this will just be adding it to the output of this step.
-
-    val sendTaskSuccessRequest = SendTaskSuccessRequest
-      .builder()
-      .taskToken(lastSateInput.taskToken)
-      .output(Json.stringify(Json.toJson(lastSateInput.input)))
-      .build()
-
-    stepFunctionsClient.sendTaskSuccess(sendTaskSuccessRequest)
-    "Done"
+    for {
+      history <- CatchAWSError(
+        stepFunctionsClient.getExecutionHistory(historyRequest)
+      )
+      events = history.events().asScala
+      event <- events
+        .find(_.`type` == HistoryEventType.TASK_SCHEDULED)
+        .toRight(s"Could not find task scheduled event for $executionId")
+    } yield {
+      val parameters = event.taskScheduledEventDetails().parameters()
+      val payload = Json.parse(parameters) \ "Payload"
+      payload.as[T]
+    }
   }
+
+  private def getVideoDimensions(
+      outputGroups: List[MediaConvertOutputGroupDetails]
+  ): Map[String, ImageAssetDimensions] = {
+    (for {
+      group <- outputGroups
+      output <- group.outputDetails
+      filename <- output.outputFilePaths.headOption
+      videoDetails <- output.videoDetails
+    } yield {
+      filename.split(".").last -> ImageAssetDimensions(
+        videoDetails.heightInPx,
+        videoDetails.widthInPx
+      )
+    }).toMap
+  }
+
+  private def applyDimensionsToAsset(
+      asset: VideoAsset,
+      videoDimensions: Map[String, ImageAssetDimensions]
+  ): VideoAsset =
+    asset match {
+      case SelfHostedAsset(sources) =>
+        val updatedSources = sources.map { source =>
+          val dimensions: Option[ImageAssetDimensions] = source.mimeType match {
+            case VideoSource.mimeTypeMp4 =>
+              videoDimensions.get("mp4")
+            case VideoSource.mimeTypeM3u8 =>
+              videoDimensions.get("m3u8")
+            case _ => None
+          }
+          source.copy(
+            width = dimensions.map(_.width),
+            height = dimensions.map(_.height)
+          )
+        }
+        SelfHostedAsset(updatedSources)
+      case _ => asset
+    }
 }
