8745885cd59f3751fa37411ed11503e01bd0a3a1
Andrew Nowak <andrew.nowak@guardian.co.uk>
2026-02-24 15:22:19 +0000

    move pluto email reminder to 'send-to-pluto' lambda since that's the only one which sent them

--- uploader/src/main/scala/com/gu/media/upload/SendToPluto.scala
+++ uploader/src/main/scala/com/gu/media/upload/SendToPluto.scala
@@ -4,6 +4,7 @@ import com.gu.media.aws.{DynamoAccess, KinesisAccess, SESSettings, SQSAccess}
 import com.gu.media.iconik.{AssetUploadedToAtomMessage, IconikUploadActions}
 import com.gu.media.lambda.{LambdaBase, LambdaWithParams}
 import com.gu.media.logging.Logging
+import com.gu.media.ses.Mailer
 import com.gu.media.upload.model.Upload
 
 class SendToPluto
@@ -16,6 +17,7 @@ class SendToPluto
     with Logging {
   private val pluto = new PlutoUploadActions(this)
   private val iconik = new IconikUploadActions(this)
+  private val mailer = new Mailer(this)
 
   override def handle(upload: Upload): Upload = {
     pluto.sendToPluto(upload.metadata.pluto)
@@ -26,6 +28,24 @@ class SendToPluto
         upload.metadata.iconikData
       )
     )
+
+    val plutoData = upload.metadata.pluto
+
+    val shouldSendEmailReminder =
+      plutoData.user != this.integrationTestUser && this.syncWithPluto
+
+    if (shouldSendEmailReminder) {
+      log.info(
+        s"Sending missing Pluto ID email user=${plutoData.user} atom=${plutoData.atomId}"
+      )
+
+      mailer.sendPlutoIdMissingEmail(
+        plutoData.atomId,
+        plutoData.title,
+        plutoData.user
+      )
+    }
+
     upload
   }
 }
