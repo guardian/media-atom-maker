59ff913ef433fa5c3065720afd297b6bef944f17
Andrew Nowak <andrew.nowak@guardian.co.uk>
2026-02-24 12:32:08 +0000

    this table looks unused; only written to and never read

--- common/src/main/scala/com/gu/media/upload/PlutoUploadActions.scala
+++ common/src/main/scala/com/gu/media/upload/PlutoUploadActions.scala
@@ -3,24 +3,14 @@ package com.gu.media.upload
 import com.gu.media.aws.{DynamoAccess, KinesisAccess}
 import com.gu.media.logging.Logging
 import com.gu.media.model._
-import com.gu.media.{PlutoDataStore, Settings}
+import com.gu.media.Settings
 
 class PlutoUploadActions(
     config: Settings with DynamoAccess with KinesisAccess
 ) extends Logging {
-  private val plutoStore =
-    new PlutoDataStore(config.scanamo, config.manualPlutoDynamo)
 
   def sendToPluto(plutoIntegrationMessage: PlutoIntegrationMessage): Unit = {
-
     sendKinesisMessage(plutoIntegrationMessage)
-
-    plutoIntegrationMessage match {
-      case plutoData: PlutoSyncMetadataMessage if plutoData.projectId.isEmpty =>
-        plutoStore.put(plutoData)
-      case _ =>
-      // there is nothing extra to do for AtomAssignedProjectMessage, PacFileMessage, or PlutoResyncMetadataMessage
-    }
   }
 
   private def sendKinesisMessage(
