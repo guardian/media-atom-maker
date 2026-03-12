a954f804d88be4c3fea1344fe3ab48966ec64806
David Furey <david.furey@guardian.co.uk>
2026-03-12 14:05:46 +0000

    Remove unused imports

--- uploader/src/main/scala/com/gu/media/upload/AddSubtitlesToMP4.scala
+++ uploader/src/main/scala/com/gu/media/upload/AddSubtitlesToMP4.scala
@@ -3,9 +3,7 @@ package com.gu.media.upload
 import com.gu.media.aws.{MediaConvertAccess, S3Access}
 import com.gu.media.lambda.{LambdaBase, LambdaWithParams}
 import com.gu.media.logging.Logging
-import com.gu.media.model.VideoSource.mimeTypeMp4
 import com.gu.media.upload.model.{SelfHostedUploadMetadata, Upload}
-import com.gu.media.util.MediaAtomHelpers.mimeType
 import software.amazon.awssdk.services.s3.model.{
   GetObjectRequest,
   PutObjectRequest
