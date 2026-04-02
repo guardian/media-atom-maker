b195779bbeb3c74508dbc1f99dd50a6670a3f1cb
Anna Beddow <anna.beddow@gmail.com>
2026-04-02 12:30:18 +0100

    Add S3Helpers trait for generating presigned download URLs.
    By generating a presigned GET URL, videos can be read directly over HTTPS. This is necessary in the immediate so that ffmpeg can stream a video before it has been transcoded to detect if a video has silent audio tracks.

---
+++ uploader/src/main/scala/com/gu/media/upload/S3Helpers.scala
@@ -0,0 +1,45 @@
+package com.gu.media.upload
+
+import software.amazon.awssdk.services.s3.presigner.S3Presigner
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest
+import software.amazon.awssdk.services.s3.model.GetObjectRequest
+import com.gu.media.aws.AwsAccess
+
+import java.time.Duration
+
+trait S3Helpers { this: AwsAccess =>
+
+  lazy val s3Presigner: S3Presigner =
+    S3Presigner
+      .builder()
+      .credentialsProvider(credentials.instance.awsV2Creds)
+      .region(awsV2Region)
+      .build()
+
+  /** Generate a presigned GET URL so videos can be read directly over HTTPS.
+    *
+    * @see
+    *   https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/examples-s3-presign.html
+    */
+  def generatePresignedDownloadUrl(
+      bucket: String,
+      key: String,
+      expiration: Duration = Duration.ofHours(1)
+  ): String = {
+    val getObjectRequest =
+      GetObjectRequest
+        .builder()
+        .bucket(bucket)
+        .key(key)
+        .build()
+
+    val presignRequest =
+      GetObjectPresignRequest
+        .builder()
+        .signatureDuration(expiration)
+        .getObjectRequest(getObjectRequest)
+        .build()
+
+    s3Presigner.presignGetObject(presignRequest).url().toExternalForm
+  }
+}
