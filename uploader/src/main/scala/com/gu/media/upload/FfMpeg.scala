3ad95c09d403a5aacbf39611d5fb7317e79bb765
Anna Beddow <anna.beddow@gmail.com>
2026-04-07 15:35:49 +0100

    Add additional checks for audio and video streams and return true or false where appropriate

--- uploader/src/main/scala/com/gu/media/upload/FfMpeg.scala
+++ uploader/src/main/scala/com/gu/media/upload/FfMpeg.scala
@@ -1,10 +1,11 @@
 package com.gu.media.upload
 
 import com.gu.media.logging.Logging
+
 import java.nio.file.Path
 import scala.sys.process.{Process, ProcessLogger, stdout}
-
 import scala.collection.mutable
+import scala.util.matching.Regex
 
 class BasicStdErrLogger extends Logging {
   private val acc = mutable.Buffer[String]()
@@ -53,7 +54,7 @@ object FfMpeg extends Logging {
       ffmpegPath: String = AWSLambdaFfmpegPath
   ): Boolean = {
 
-    // @see https://aws.amazon.com/blogs/media/detect-silent-audio-tracks-in-vod-content-with-aws-elemental-mediaconvert/
+    /* @see https://aws.amazon.com/blogs/media/detect-silent-audio-tracks-in-vod-content-with-aws-elemental-mediaconvert/ */
     val SILENT_THRESHOLD = -50
 
     val ffMpegStdErrLogger = new BasicStdErrLogger()
@@ -64,22 +65,36 @@ object FfMpeg extends Logging {
       ProcessLogger(stdout.append(_), ffMpegStdErrLogger.append)
     )
 
+    def findRegexMatch(regex: Regex, output: String) = {
+      regex
+        .findFirstMatchIn(output)
+    }
+
     exitCode match {
       case 0 =>
         val output = ffMpegStdErrLogger.getOutput;
-        val maxVolumeRegex = """max_volume:\s*([-\d.]+)\s*dB""".r
-        val maxVolume = maxVolumeRegex
-          .findFirstMatchIn(output)
-          .map(_.group(1).toDouble)
 
-        maxVolume match {
-          case Some(db) =>
-            db > SILENT_THRESHOLD // treat near-silence as no audio
-          case None => false // couldn't parse = assume no audio
+        val maxVolumeRegex = """max_volume:\s*([-\d.]+)\s*dB""".r
+        val audioStreamRegex = """Stream .*: Audio""".r
+        val videoStreamRegex = """Stream .*: Video""".r
+
+        val maxVolume = findRegexMatch(maxVolumeRegex, output)
+          .map(_.group(1).toDouble)
+        val audioStream = findRegexMatch(audioStreamRegex, output)
+        val videoStream = findRegexMatch(videoStreamRegex, output)
+
+        (maxVolume, audioStream, videoStream) match {
+
+          case (Some(db), _, _) =>
+            db > SILENT_THRESHOLD /* We treat near-silence as no audio */
+          case (None, None, Some(video)) =>
+            false /* Couldn't parse volume, audio streams are absent, but video streams are present. We assume from this that the video is silent */
+          case (None, None, None) =>
+            true /* Couldn't parse volume and there are no audio or video streams, so we return true for safety*/
         }
       case _ =>
         log.error("FfMpeg audio detection failed")
-        true // audio detection failure is not a critical error, so we return true rather than throwing an exception
+        true /* Audio detection failure is not a critical error, so we return true rather than throwing an exception */
     }
   }
 
