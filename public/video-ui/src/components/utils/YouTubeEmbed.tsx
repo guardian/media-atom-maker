d0daa2e425e45848b0fd93a1795f570cd9287271
Anna Beddow <anna.beddow@gmail.com>
2026-04-09 11:46:08 +0100

    Add types

--- public/video-ui/src/components/utils/YouTubeEmbed.tsx
+++ public/video-ui/src/components/utils/YouTubeEmbed.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { getStore } from '../../util/storeAccessor';
 
 
-export const getYouTubeEmbedUrl = (id) => {
+export const getYouTubeEmbedUrl = (id: string) => {
   const embedUrl = getStore().getState().config.youtubeEmbedUrl;
   return `${embedUrl}${id}?showinfo=0&rel=0`;
 };
-export function YouTubeEmbed({ id, className, largePreview }) {
+export function YouTubeEmbed({ id, className, largePreview}: { id: string; className: string, largePreview: boolean } ) {
   return (
     <iframe
       className={className}
