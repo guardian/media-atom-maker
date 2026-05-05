a15bef1be10566aabacfbf3e3384953cdc83f98d
lindseydew <lindsey.dew@guardian.co.uk>
2026-05-05 18:13:35 +0100

    Accumulate the entered / exited states

--- app/util/Metrics.scala
+++ app/util/Metrics.scala
@@ -10,9 +10,9 @@ import software.amazon.awssdk.services.sfn.model.{
 import scala.jdk.CollectionConverters.ListHasAsScala
 
 class Metrics(telemetry: Telemetry, stepFunctions: StepFunctions) {
-
+  val LAMBA_WARM_UP = "LAMBA_WARM_UP"
   def computeDurations(historyEvents: List[HistoryEvent]) = {
-    val taskEnteredMap = historyEvents.foldLeft(Map[String, Long]())({
+    val taskEnteredMap = historyEvents.foldLeft(Map[String, List[Long]]())({
       case (acc, h) =>
         val ts = h.timestamp().toEpochMilli
         h.`type`() match {
@@ -25,46 +25,81 @@ class Metrics(telemetry: Telemetry, stepFunctions: StepFunctions) {
             Option(h.stateEnteredEventDetails())
               .map(_.name())
               .fold(acc)(name => {
-                acc.updated(name, ts)
+                val previousTimestamps = acc.getOrElse(name, Nil)
+                acc.updated(name, ts :: previousTimestamps)
               })
           case HistoryEventType.LAMBDA_FUNCTION_SCHEDULED =>
-            acc.updated("LAMBA_WARM_UP", ts)
+            val previousTimestamps = acc.getOrElse(LAMBA_WARM_UP, Nil)
+            acc.updated(LAMBA_WARM_UP, ts :: previousTimestamps)
           case _ => acc
         }
     })
 
-    historyEvents.foldLeft(Map[String, Long]())({ case (acc, h) =>
-      val ts = h.timestamp().toEpochMilli
-      h.`type`() match {
-        case HistoryEventType.TASK_STATE_EXITED |
-            HistoryEventType.WAIT_STATE_EXITED |
-            HistoryEventType.PASS_STATE_EXITED |
-            HistoryEventType.CHOICE_STATE_EXITED |
-            HistoryEventType.PARALLEL_STATE_EXITED |
-            HistoryEventType.MAP_STATE_EXITED =>
-          (for {
-            eventDetails <- Option(h.stateExitedEventDetails())
-            name = eventDetails.name()
-            start <- taskEnteredMap.get(name)
-            duration = ts - start
-          } yield {
-            acc.updated(name, duration)
-          }).getOrElse(acc)
+    val taskExited = historyEvents.foldLeft(Map[String, List[Long]]())({
+      case (acc, h) =>
+        val ts = h.timestamp().toEpochMilli
+        h.`type`() match {
+          case HistoryEventType.TASK_STATE_EXITED |
+              HistoryEventType.WAIT_STATE_EXITED |
+              HistoryEventType.PASS_STATE_EXITED |
+              HistoryEventType.CHOICE_STATE_EXITED |
+              HistoryEventType.PARALLEL_STATE_EXITED |
+              HistoryEventType.MAP_STATE_EXITED =>
+            Option(h.stateExitedEventDetails())
+              .map(_.name())
+              .fold(acc)(name => {
+                val previousTimestamps = acc.getOrElse(name, Nil)
+                acc.updated(name, ts :: previousTimestamps)
+              })
 
-        case HistoryEventType.LAMBDA_FUNCTION_STARTED => {
-          taskEnteredMap
-            .get("LAMBA_WARM_UP")
-            .fold(acc)(start => {
-              acc.updated("LAMBA_WARM_UP", ts - start)
-            })
+          case HistoryEventType.LAMBDA_FUNCTION_STARTED =>
+            val previousTimestamps = acc.getOrElse(LAMBA_WARM_UP, Nil)
+            acc.updated(LAMBA_WARM_UP, ts :: previousTimestamps)
+          case _ => acc
         }
-        case _ => acc
-      }
     })
+    val entered = taskEnteredMap.view.mapValues(ls => ls.sorted)
+    val exited = taskExited.view.mapValues(ls => ls.sorted)
+    exited.foldLeft(Map[String, Long]())({
+      case (acc, (name, exitedDurations)) =>
+        val enteredDurations = entered.getOrElse(name, Nil)
+        val totalTimes = exitedDurations
+          .zip(enteredDurations)
+          .map({ case (finish, start) => finish - start })
+          .sum
+        acc.updated(name, totalTimes)
+    })
+
   }
 
   def getStepIdFromArn(arn: String) = {
     arn.split(":").toList.reverse.headOption
+  }
+
+  def executionArn(stepId: String) = {
+    s"arn:aws:states:eu-west-1:563563610310:execution:VideoPipelineDEV-PGZ5E0CNI0QG:$stepId"
+  }
+
+  def generateMetrics(
+      executionArn: String,
+      runTime: Long
+  ): Map[String, TagValue] = {
+    val historyEvents =
+      stepFunctions.getAllHistoryEvents(executionArn)
+
+    val durationsMap = computeDurations(historyEvents.toList)
+    val totalSubTimes = durationsMap.foldLeft(0L)({ case (acc, (_, v)) =>
+      acc + v
+    })
+    val stepId = getStepIdFromArn(executionArn).getOrElse(executionArn)
+    Map(
+      "stepId" -> TagString(stepId),
+      "jobTime" -> TagNumber(runTime),
+      "totalSubTimes" -> TagNumber(totalSubTimes),
+      "duration_unknown" -> TagNumber(runTime - totalSubTimes)
+    ) ++ durationsMap.map({ case (k, v) =>
+      (s"duration_${k}", TagNumber(v))
+    })
   }
 
   def run(): Unit = {
@@ -73,20 +108,12 @@ class Metrics(telemetry: Telemetry, stepFunctions: StepFunctions) {
       .getPreviousExecutions(20)
       .foreach(event => {
         val arn = event.executionArn()
-        val runtime =
+
+        val runTime =
           event.stopDate().toEpochMilli - event.startDate().toEpochMilli
-        val historyEvents =
-          stepFunctions.getAllHistoryEvents(event.executionArn())
-        val durationsMap = computeDurations(historyEvents.toList)
-        val totalSubTimes = durationsMap.foldLeft(0L)({case (acc, (_, v)) => acc + v})
-        val stepId = getStepIdFromArn(arn).getOrElse(arn)
-        val tags = Map(
-          "stepId" -> TagString(stepId),
-          "jobTime" -> TagNumber(runtime),
-          "totalSubTimes" -> TagNumber(totalSubTimes),
-        ) ++ durationsMap.map({ case (k, v) =>
-          (s"duration_${k}", TagNumber(v))
-        })
+
+        val tags = generateMetrics(arn, runTime)
+
         telemetry.sendTelemetryEvent(
           "test",
           tags
