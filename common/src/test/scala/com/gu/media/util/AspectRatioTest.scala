736cc619d21360a0b87acbb0644b21f2e7053678
David Furey <david.furey@guardian.co.uk>
2026-05-19 12:05:23 +0100

    More primitive aspect ratio calculation
    The existing solution is quite complicated. It attempts to express the aspect ratio in the standard form x:y, allowing a supplied pixel tolerance to match to pre-defined ratios.
    
    For our purposes, it is more helpful to assume that the video creator was trying to create a video that matched one of our ratios, but with the constraint that the width (or height) had to be fixed to a specific size. This more closely matches how our tool is used.

--- common/src/test/scala/com/gu/media/util/AspectRatioTest.scala
+++ common/src/test/scala/com/gu/media/util/AspectRatioTest.scala
@@ -7,25 +7,36 @@ class AspectRatioTest extends AnyFlatSpec with Matchers {
 
   "calculate" should "produce an aspect ratio from dimensions" in {
     AspectRatio.calculate(1280, 720).map(_.name) should contain("16:9")
+    AspectRatio.calculate(1280, 720).map(_.name) should contain("16:9")
     AspectRatio.calculate(720, 1280).map(_.name) should contain("9:16")
     AspectRatio.calculate(1024, 1280).map(_.name) should contain("4:5")
     AspectRatio.calculate(1280, 1024).map(_.name) should contain("5:4")
   }
 
   "calculate" should "apply a tolerance to match inexact dimensions" in {
-    AspectRatio.calculate(1280, 720, tolerance = 3).map(_.name) should contain(
+    AspectRatio.calculate(480, 854, tolerance = 0).map(_.name) should contain(
+      "9:16"
+    )
+    AspectRatio.calculate(480, 855, tolerance = 1).map(_.name) should contain(
+      "9:16"
+    )
+
+    AspectRatio.calculate(1280, 720, tolerance = 0).map(_.name) should contain(
       "16:9"
     )
-    AspectRatio.calculate(1280, 721, tolerance = 3).map(_.name) should contain(
+    AspectRatio.calculate(1280, 721, tolerance = 0) shouldBe empty
+    AspectRatio.calculate(1280, 721, tolerance = 1).map(_.name) should contain(
       "16:9"
     )
-    AspectRatio.calculate(1280, 722, tolerance = 3).map(_.name) should contain(
+    AspectRatio.calculate(1280, 722, tolerance = 1) shouldBe empty
+    AspectRatio.calculate(1280, 722, tolerance = 2).map(_.name) should contain(
       "16:9"
     )
+    AspectRatio.calculate(1280, 723, tolerance = 2) shouldBe empty
     AspectRatio.calculate(1280, 723, tolerance = 3).map(_.name) should contain(
       "16:9"
     )
-    AspectRatio.calculate(1280, 724, tolerance = 3).map(_.name) shouldBe empty
+    AspectRatio.calculate(1280, 724, tolerance = 3) shouldBe empty
   }
 
 }
