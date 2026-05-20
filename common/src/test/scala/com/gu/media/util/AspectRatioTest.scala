f0e30c94eef209042f11478e8b6178cae1a75010
David Furey <david.furey@guardian.co.uk>
2026-05-20 09:48:51 +0100

    Remove duplicated test

--- common/src/test/scala/com/gu/media/util/AspectRatioTest.scala
+++ common/src/test/scala/com/gu/media/util/AspectRatioTest.scala
@@ -7,7 +7,6 @@ class AspectRatioTest extends AnyFlatSpec with Matchers {
 
   "calculate" should "produce an aspect ratio from dimensions" in {
     AspectRatio.calculate(1280, 720).map(_.name) should contain("16:9")
-    AspectRatio.calculate(1280, 720).map(_.name) should contain("16:9")
     AspectRatio.calculate(720, 1280).map(_.name) should contain("9:16")
     AspectRatio.calculate(1024, 1280).map(_.name) should contain("4:5")
     AspectRatio.calculate(1280, 1024).map(_.name) should contain("5:4")
