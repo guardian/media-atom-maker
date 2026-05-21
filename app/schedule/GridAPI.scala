f6fc3b16b1d95d7d2df4f795103183f707d98900
lindseydew <lindsey.dew@guardian.co.uk>
2026-05-21 16:57:44 +0100

    Call the grid api

--- app/schedule/GridAPI.scala
+++ app/schedule/GridAPI.scala
@@ -9,6 +9,7 @@ import java.util.concurrent.atomic.AtomicReference
 import javax.inject._
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
+import scala.util.{Failure, Success}
 
 @Singleton
 class GridAPI @Inject()(actorSystem: ActorSystem, ws: WSClient)(
@@ -27,40 +28,33 @@ class GridAPI @Inject()(actorSystem: ActorSystem, ws: WSClient)(
   def getCropOptions: List[CropOption] = agent.get()
 
   private def runJob(): Unit = {
-    log.info("setting agents")
-    agent.set(List(
-      CropOption("landscape", "5 / 4", "5:4"),
-      CropOption("portrait", "4 / 5", "4:5"),
-      CropOption("video", "16 / 9", "16:9"),
-      CropOption("square", "1", "1:1"),
-    ))
-//    ws.url(
-//      "https://api.media.test.dev-gutools.co.uk/configuration/crop-variations"
-//    ).withHttpHeaders(
-//      (
-//        "X-Gu-Media-Key",
-//        "media-atom-maker-vCClN4aOIL6qRRSVK42ikalzQwfLhfk2n4b4YSHw0tklq1pw"
-//      )
-//    ).get() onComplete {
-//      case Success(value) =>
-//        value.json
-//          .validate[List[CropOption]]
-//          .asOpt
-//          .fold(
-//            log.error(
-//              "Could not parse response from grid API for crop variations"
-//            )
-//          )(cropOptions => {
-//            log.info(
-//              "Updating crop options cache"
-//            )
-//            agent.set(cropOptions)
-//          })
-//      case Failure(exception) =>
-//        log.error(
-//          s"Error calling the grid API for crop variations ${exception.getMessage}"
-//        )
-//    }
+    ws.url(
+      "https://api.media.test.dev-gutools.co.uk/configuration/crop-variations"
+    ).withHttpHeaders(
+      (
+        "X-Gu-Media-Key",
+        "media-atom-maker-vCClN4aOIL6qRRSVK42ikalzQwfLhfk2n4b4YSHw0tklq1pw"
+      )
+    ).get() onComplete {
+      case Success(value) =>
+        value.json
+          .validate[List[CropOption]]
+          .asOpt
+          .fold(
+            log.error(
+              "Could not parse response from grid API for crop variations"
+            )
+          )(cropOptions => {
+            log.info(
+              "Updating crop options cache"
+            )
+            agent.set(cropOptions)
+          })
+      case Failure(exception) =>
+        log.error(
+          s"Error calling the grid API for crop variations ${exception.getMessage}"
+        )
+    }
 
   }
 }
