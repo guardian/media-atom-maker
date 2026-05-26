8e0bb8e8d47dcb9d082be33585f70bc576c3cec5
lindseydew <lindsey.dew@guardian.co.uk>
2026-05-26 15:33:18 +0100

    Run the formatter

--- uploader/src/main/scala/com/gu/media/upload/JobMetricsPublisher.scala
+++ uploader/src/main/scala/com/gu/media/upload/JobMetricsPublisher.scala
@@ -3,8 +3,10 @@ package com.gu.media.upload
 import com.gu.media.lambda.{LambdaBase, LambdaWithParams}
 import com.gu.media.logging.Logging
 
-class JobMetricsPublisher extends LambdaWithParams[String, String]
-  with LambdaBase with Logging  {
+class JobMetricsPublisher
+    extends LambdaWithParams[String, String]
+    with LambdaBase
+    with Logging {
 
   override def handle(input: String): String = {
     log.info("helloworld")
