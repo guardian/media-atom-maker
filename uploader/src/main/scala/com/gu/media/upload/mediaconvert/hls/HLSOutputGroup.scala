4b9e63dbde68b42429992fae5726e94ef965809b
Anna Beddow <anna.beddow@gmail.com>
2026-07-01 09:22:30 +0100

    Revert "Revert "Define encoding config less verbosely""

--- uploader/src/main/scala/com/gu/media/upload/mediaconvert/hls/HLSOutputGroup.scala
+++ uploader/src/main/scala/com/gu/media/upload/mediaconvert/hls/HLSOutputGroup.scala
@@ -2,7 +2,12 @@ package com.gu.media.upload.mediaconvert.hls
 
 import com.gu.contentatom.thrift.atom.media.AssetType
 import com.gu.media.model.VideoSource
-import com.gu.media.upload.mediaconvert.{EncodingConfigs, OutputGroupDefinition}
+import com.gu.media.upload.mediaconvert.{
+  Dimensions,
+  EncodingConfig,
+  H264EncodingConfig,
+  OutputGroupDefinition
+}
 import software.amazon.awssdk.services.mediaconvert.model.{
   CmafGroupSettings,
   CmafWriteDASHManifest,
@@ -12,16 +17,29 @@ import software.amazon.awssdk.services.mediaconvert.model.{
 }
 
 object HLSOutputGroup {
+
+  private val commonDimensions = List(
+    Dimensions(Some(480), None),
+    Dimensions(None, Some(720))
+  )
+
+  private val outputDefinitions: List[
+    ((Dimensions, Int) => EncodingConfig, List[Dimensions], List[Int])
+  ] = List(
+    (H264EncodingConfig, commonDimensions, List(8, 6, 4))
+  )
+
+  private val videoOutputs = for {
+    (encodingConfig, dimensions, qualities) <- outputDefinitions
+    dimension <- dimensions
+    quality <- qualities
+  } yield VideoOutput(encodingConfig(dimension, quality))
+
   def apply(hasAudio: Boolean): OutputGroupDefinition = {
-    val outputs = List(
-      VideoOutput(EncodingConfigs.MobileWidth),
-      VideoOutput(EncodingConfigs.Default),
-      VideoOutput(EncodingConfigs.LowQuality),
-      VideoOutput(EncodingConfigs.LowQualityMobileWidth),
-      VideoOutput(EncodingConfigs.VeryLowQuality),
-      VideoOutput(EncodingConfigs.VeryLowQualityMobileWidth),
-      CaptionsOutput()
-    ) ++ (if (hasAudio) List(AudioOutput()) else Nil)
+    val outputs = videoOutputs ++
+      List(CaptionsOutput()) ++
+      List(AudioOutput()).filter(_ => hasAudio)
+
     OutputGroupDefinition(
       mimeType = Some(VideoSource.mimeTypeM3u8),
       assetType = Some(
