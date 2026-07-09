2702ea72964861d309f285f0e521cb8bec12b2a7
AlexAscherson <aascherson@gmail.com>
2026-07-09 12:28:17 +0100

    Update file to typescript
    Adds DisplayTag and CapiTagResponse types
    
    Checks if api response is valid
    
    Removes unecessary else

---
+++ public/video-ui/src/util/tagParsers.ts
@@ -0,0 +1,72 @@
+import ContentApi from '../services/capi';
+import TagTypes from '../constants/TagTypes';
+
+type DisplayTag = {
+  id: string;
+  webTitle: string;
+};
+
+type CapiTagResponse = {
+  response?: {
+    tag?: DisplayTag;
+  };
+};
+
+export type ParsedTag = string | DisplayTag;
+
+const isCapiTagResponse = (value: unknown): value is CapiTagResponse => {
+  if (!value || typeof value !== 'object') {
+    return false;
+  }
+
+  const response = (value as CapiTagResponse).response;
+  return !response || !response.tag || typeof response.tag.id === 'string';
+};
+
+export function tagsFromStringList(
+  savedTags: string[],
+  tagType: string
+): Promise<ParsedTag[]> {
+  if (!savedTags) {
+    return Promise.resolve([]);
+  }
+
+  return Promise.all(
+    savedTags.map((element): Promise<ParsedTag> => {
+      if (
+        (tagType !== TagTypes.contributor && tagType !== TagTypes.youtube) ||
+        element.match('^profile/')
+      ) {
+        return ContentApi.getLivePage(element).then(response => {
+          if (isCapiTagResponse(response) && response.response?.tag) {
+            return response.response.tag;
+          }
+
+          return {
+            id: element,
+            webTitle: element
+          };
+        });
+      }
+
+      if (tagType === TagTypes.youtube) {
+        return Promise.resolve({
+          id: element,
+          webTitle: element
+        });
+      }
+
+      return Promise.resolve(element);
+    })
+  );
+}
+
+export function tagsToStringList(addedTags: ParsedTag[]): string[] {
+  return addedTags.map(tag => {
+    if (typeof tag === 'string') {
+      return tag;
+    }
+
+    return tag.id;
+  });
+}
