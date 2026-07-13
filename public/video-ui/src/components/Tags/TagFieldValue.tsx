169ee66489eb7370010fad502ce1943ad9a5029b
AlexAscherson <aascherson@gmail.com>
2026-07-13 11:18:54 +0100

    Update TagFieldValue to ts

--- public/video-ui/src/components/Tags/TagFieldValue.tsx
+++ public/video-ui/src/components/Tags/TagFieldValue.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 
-export default class TagFieldValue extends React.Component {
-  renderFieldValue(value, index, array) {
+type TagFieldValueItem =
+  | string
+  | {
+      id: string;
+      detailedTitle?: string;
+      webTitle?: string;
+    };
+
+type Props = {
+  tagType?: string;
+  tagValue: TagFieldValueItem[];
+};
+
+export default class TagFieldValue extends React.Component<Props> {
+  renderFieldValue(
+    value: TagFieldValueItem,
+    index: number,
+    array: TagFieldValueItem[]
+  ): React.ReactNode {
     // Add a trailing comma if it's a youtube keyword field and it's not the last keyword in the array
     const shouldAddComma =
-      this?.props?.tagType === 'youtube' && index !== array.length - 1;
-    if (value.detailedTitle) {
+      this.props.tagType === 'youtube' && index !== array.length - 1;
+
+    if (typeof value !== 'string' && value.detailedTitle) {
       return (
         <span key={`${value.id}-${index}`}>
           <span className="form__field__tag__display">
@@ -14,9 +32,9 @@ export default class TagFieldValue extends React.Component {
           {shouldAddComma ? ',' : ' '}
         </span>
       );
-    } else if (value.webTitle) {
-      // In a  YouTube Furniture tab with non-launched changes, the `keyword` field
-      //passes an object with webTitle to this component
+    } else if (typeof value !== 'string' && value.webTitle) {
+      // In a YouTube Furniture tab with non-launched changes, the `keyword` field
+      // passes an object with webTitle to this component
       const titleToRender = value.webTitle.concat(shouldAddComma ? ',' : '');
       return titleToRender;
     }
@@ -27,7 +45,8 @@ export default class TagFieldValue extends React.Component {
 
     return ` ${value}`;
   }
-  getRenderedValues() {
+
+  getRenderedValues(): React.ReactNode[] {
     return this.props.tagValue.map((tagValue, index, array) =>
       this.renderFieldValue(tagValue, index, array)
     );
