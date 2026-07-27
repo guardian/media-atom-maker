6980f4d9c355626ec757002dd0387d6a30dc7e5a
AlexAscherson <aascherson@gmail.com>
2026-07-27 14:14:26 +0100

    Add sentry to cover scala bits of the app
    Fixes no scala coverage

--- app/util/RequestLogging.scala
+++ app/util/RequestLogging.scala
@@ -1,6 +1,7 @@
 package util
 
 import com.gu.media.logging.Logging
+import io.sentry.Sentry
 
 import javax.inject.{Inject, Provider, Singleton}
 import play.api.http.DefaultHttpErrorHandler
@@ -20,10 +21,47 @@ class RequestLogging @Inject() (
 ) extends DefaultHttpErrorHandler(env, config, sourceMapper, router)
     with Logging {
 
+  private val stage = config.getOptional[String]("stage").getOrElse("DEV")
+  private val sentryDsn = config.getOptional[String]("raven.url").getOrElse("")
+  private val sentryEnabled =
+    sentryDsn.nonEmpty && stage != "DEV"
+
+  if (sentryEnabled) {
+    Sentry.init(options => {
+      options.setDsn(sentryDsn)
+      options.setEnvironment(stage)
+      options.setAttachStacktrace(true)
+    })
+  }
+
   override def onProdServerError(
       request: RequestHeader,
       exception: UsefulException
   ): Future[Result] = {
+    if (sentryEnabled) {
+      Sentry.withScope(scope => {
+        scope.setTag("http.method", request.method)
+        scope.setTag("http.path", request.path)
+        scope.setTag("http.host", request.host)
+        scope.setTag("http.status_code", "500")
+        scope.setTag("stage", stage)
+        scope.setExtra("request.uri", request.uri)
+        scope.setExtra("request.queryString", request.rawQueryString)
+        scope.setExtra("request.remoteAddress", request.remoteAddress)
+        scope.setExtra(
+          "request.userAgent",
+          request.headers.get("User-Agent").getOrElse("")
+        )
+        scope.setExtra(
+          "request.id",
+          request.headers
+            .get("X-Request-Id")
+            .orElse(request.headers.get("x-request-id"))
+            .getOrElse("")
+        )
+        Sentry.captureException(exception)
+      })
+    }
     super.logServerError(request, exception)
     super.onProdServerError(request, exception)
   }
