3ba0d5644b7d04d85506eff87e397cbd2262c543
hk-15 <168672047+hk-15@users.noreply.github.com>
2026-08-28 15:30:24 +0100

    Update config and set up scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -34,7 +34,11 @@ install_deps_and_build() {
 main() {
   check_node_version
   install_yarn
-  setup_nginx
+  if [ "$USER" = "vscode" ]; then
+    echo "Running in VS Code dev containers, skipping nginx setup"
+  else
+    setup_nginx
+  fi
   install_deps_and_build
   printf "\n\rDone.\n\r\n\r"
 }
